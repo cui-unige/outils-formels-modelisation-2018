eae9f99d9b510cc13ebe6e30a830f89bd72d8b8d
Marc Heimendinger <marc@Marcs-MacBook-Pro.local>
2018-10-05 13:17:54 +0200

    ex-02 Update 1

--- ex-02/Sources/Mutex/main.swift
+++ ex-02/Sources/Mutex/main.swift
@@ -1,31 +1,36 @@
 private func pre(p: Place, t: Transition) -> Nat {
   switch (p, t) {
-  case (Place("p1"), Transition("t1")): return 2
-  case (Place("p1"), Transition("t2")): return 1
-  case (Place("p1"), Transition("t3")): return 0
-  case (Place("p2"), Transition("t1")): return 0
-  case (Place("p2"), Transition("t2")): return 6
-  case (Place("p2"), Transition("t3")): return 4
+  case (Place("s0"), Transition("t0")): return 1
+  case (Place("s1"), Transition("t1")): return 1
+  case (Place("s2"), Transition("t2")): return 1
+  case (Place("s3"), Transition("t3")): return 1
+  case (Place("s4"), Transition("t0")): return 1
+  case (Place("s4"), Transition("t2")): return 1
+  case (Place("b"), Transition("t2")): return 1
+  case (Place("c"), Transition("t0")): return 1
   default: return 0
   }
 }
 
 private func post(p: Place, t: Transition) -> Nat {
   switch (p, t) {
-  case (Place("p1"), Transition("t1")): return 5
-  case (Place("p1"), Transition("t2")): return 0
-  case (Place("p1"), Transition("t3")): return 1
-  case (Place("p2"), Transition("t1")): return 7
-  case (Place("p2"), Transition("t2")): return 3
-  case (Place("p2"), Transition("t3")): return 0
+  case (Place("s1"), Transition("t0")): return 1
+  case (Place("b"), Transition("t0")): return 1
+  case (Place("s0"), Transition("t1")): return 1
+  case (Place("s4"), Transition("t1")): return 1
+  case (Place("c"), Transition("t2")): return 1
+  case (Place("c"), Transition("t2")): return 1
+  case (Place("s1"), Transition("t0")): return 1
+  case (Place("s2"), Transition("t3")): return 1
+  case (Place("s4"), Transition("t3")): return 1
   default: return 0
   }
 }
 
 func initialMarking(_ place: Place) -> Nat {
   switch place {
-  case Place("p1"): return 2
-  case Place("p2"): return 3
+  case Place("c"): return 3
+  case Place("s4"): return 1
   default: return 0
   }
 }
