ee062cb304aedc35175e245a40deeb1b3e2f1df5
Rose <rose@localhost.localdomain>
2018-10-09 13:18:17 +0200

    debut exo 3 et 4

--- tp-01/Sources/SemaLib/CounterModel.swift
+++ tp-01/Sources/SemaLib/CounterModel.swift
@@ -1,6 +1,26 @@
 /// This function creates the model of a binary counter on three bits.
 public func createCounterModel() -> PetriNet {
   // Write your code here.
+  let places :Place = [Place("b0"),Place("b1"),Place("b2"),
+  Place("p1"),Place("p1"),Place("p2"),Place("p3"),Place("p4")]
+
+  let transitions: Transition = [Transition("t0"), Transition("t1")
+  ,Transition("t2"), Transition("t3"),Transition("t4"), Transition("t5")]
+  func pre(_ p: Place, _ t: Transition) -> Nat{
+      switch (p,t) {
+      case (Place("p1"),Transition("t0")): return 1
+      case (Place("b0"),Transition("")): return 1
+
+      default: return 0
+      }
+  }
+  func post(_ p: Place, _ t: Transition) -> Nat {
+    switch (p,t) {
+    case (Place("b0"),Transition("t0")): return 1
+    case (Place("p1"),Transition("t0")): return 1
+    default: return 0
+    }
+  }
   return PetriNet(places: [], transitions: [], pre: { _, _ in 0 }, post: { _, _ in 0 })
 }
 
