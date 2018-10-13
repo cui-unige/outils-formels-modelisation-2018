95212a5a63ddee9875c02ec226d71ef21aa8ff0f
Rose <rose@pc86.home>
2018-10-13 09:56:21 +0200

    TP1 finit

--- tp-01/Sources/SemaLib/CounterModel.swift
+++ tp-01/Sources/SemaLib/CounterModel.swift
@@ -1,31 +1,51 @@
 /// This function creates the model of a binary counter on three bits.
 public func createCounterModel() -> PetriNet {
   // Write your code here.
-  let places :Place = [Place("b0"),Place("b1"),Place("b2"),
-  Place("p1"),Place("p1"),Place("p2"),Place("p3"),Place("p4")]
-
-  let transitions: Transition = [Transition("t0"), Transition("t1")
-  ,Transition("t2"), Transition("t3"),Transition("t4"), Transition("t5")]
-  func pre(_ p: Place, _ t: Transition) -> Nat{
-      switch (p,t) {
-      case (Place("p1"),Transition("t0")): return 1
-      case (Place("b0"),Transition("")): return 1
-
-      default: return 0
-      }
-  }
-  func post(_ p: Place, _ t: Transition) -> Nat {
-    switch (p,t) {
-    case (Place("b0"),Transition("t0")): return 1
-    case (Place("p1"),Transition("t0")): return 1
+  func pre(p: Place, t: Transition) -> Nat {
+    switch (p, t) {
+    case (Place("p0"), Transition("t0")): return 1
+    case (Place("p1"), Transition("t1")): return 1
+    case (Place("b0"), Transition("t1")): return 1
+    case (Place("p2"), Transition("t2")): return 1
+    case (Place("b1"), Transition("t2")): return 1
+    case (Place("b0"), Transition("t3")): return 1
+    case (Place("b1"), Transition("t3")): return 1
+    case (Place("b2"), Transition("t3")): return 1
     default: return 0
     }
   }
-  return PetriNet(places: [], transitions: [], pre: { _, _ in 0 }, post: { _, _ in 0 })
+  func post(p: Place, t: Transition) -> Nat {
+    switch (p, t) {
+    case (Place("b0"), Transition("t0")): return 1
+    case (Place("b1"), Transition("t1")): return 1
+    case (Place("p0"), Transition("t1")): return 1
+    case (Place("b2"), Transition("t2")): return 1
+    case (Place("p1"), Transition("t2")): return 1
+    case (Place("p0"), Transition("t3")): return 1
+    case (Place("p1"), Transition("t3")): return 1
+    case (Place("p2"), Transition("t3")): return 1
+    default: return 0
+    }
+  }
+  let compteur_binaire = PetriNet(
+    places : [Place("b0"), Place("b1"), Place("b2"),Place("p0"), Place("p1"), Place("p2")],
+    transitions : [Transition("t0"),Transition("t1"),Transition("t2"), Transition("t3")],
+    pre : pre,
+    post : post)
+
+  return compteur_binaire
 }
 
 /// This function returns the initial marking corresponding to the model of your binary counter.
 public func createCounterInitialMarking() -> Marking {
   // Write your code here.
-  return { _ in 0 }
+  func initialMarking(_ place: Place) -> Nat {
+    switch place {
+      case Place("p0"): return 1
+      case Place("p1"): return 1
+      case Place("p2"): return 1
+      default: return 0
+    }
+  }
+  return initialMarking
 }
