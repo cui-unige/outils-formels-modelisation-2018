02ce8b3f033a234a0e47e5edc9d3cd4105f71944
elias <eliaas01210@gmail.com>
2018-10-15 22:17:36 +0200

    [TP1]

--- tp-01/Sources/SemaLib/PetriNet.swift
+++ tp-01/Sources/SemaLib/PetriNet.swift
@@ -28,19 +28,27 @@ public struct PetriNet {
   public let post: (Place, Transition) -> Nat
 
   /// A method that returns whether a transition is fireable from a given marking.
-  public func isFireable(_ transition: Transition, from marking: Marking) -> Bool {
-    // Write your code here.
-    return false
-  }
+    public func isFireable(_ transition: Transition, from marking: Marking) -> Bool {
+        for place in places{
+            if(marking(place) < pre(place, transition)){
+                return false
+            }
+        }
+        return true
+    }
 
-  /// A method that fires a transition from a given marking.
-  ///
-  /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
-  /// otherwise it returns the new marking.
-  public func fire(_ transition: Transition, from marking: @escaping Marking) -> Marking? {
-    // Write your code here.
-    return nil
-  }
+    /// A method that fires a transition from a given marking.
+    ///
+    /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
+    /// otherwise it returns the new marking.
+    public func fire(_ transition: Transition, from marking: @escaping Marking) -> Marking? {
+        if(self.isFireable(transition, from: marking)){
+            return {(place) -> Nat in
+                return marking(place) - self.pre(place,transition) + self.post(place,transition)
+            }
+        }
+        return nil
+    }
 
   /// A helper function to print markings.
   public func print(marking: Marking) {
