5fa824f93dc4037ce3fc2e00ce6b0b022d38dcad
Karma <tawilkarma@gmail.com>
2018-10-15 23:08:44 +0200

    Changement du réseau de Petri afin qu'il fonctionne correctement grâce à la création de place conditions

--- tp-01/Sources/SemaLib/CounterModel.swift
+++ tp-01/Sources/SemaLib/CounterModel.swift
@@ -2,36 +2,42 @@
 public func createCounterModel() -> PetriNet {
   func pre(p: Place, t: Transition) -> Nat {
     switch(p, t) {
-      case(Place("b0"), Transition("t1")) : return 1
-      case(Place("b0"), Transition("t3")) : return 1
-      case(Place("b0"), Transition("t5")) : return 1
+      case(Place("c0"), Transition("t1")): return 1
 
-      case(Place("b1"), Transition("t2")) : return 1
-      case(Place("b1"), Transition("t3")) : return 1
-      case(Place("b1"), Transition("t5")) : return 1
+      case(Place("b0"), Transition("t2")): return 1
+      case(Place("b0"), Transition("t4")): return 1
 
-      case(Place("b2"), Transition("t4")) : return 1
-      case(Place("b2"), Transition("t5")) : return 1
+      case(Place("b1"), Transition("t3")): return 1
+      case(Place("b1"), Transition("t4")): return 1
+
+      case(Place("c1"), Transition("t2")): return 1
+
+      case(Place("b2"), Transition("t4")): return 1
+
+      case(Place("c2"), Transition("t3")):return 1
       default: return 0
     }
   }
   func post(p: Place, t: Transition) -> Nat {
    switch(p, t) {
-      case(Place("b0"), Transition("t2")): return 1
-      case(Place("b0"), Transition("t4")): return 1
+      case(Place("b0"), Transition("t1")): return 1
 
-      case(Place("b1"), Transition("t1")): return 1
       case(Place("b1"), Transition("t2")): return 1
+      case(Place("c0"), Transition("t2")): return 1
 
+      case(Place("c1"), Transition("t3")): return 1
       case(Place("b2"), Transition("t3")): return 1
-      case(Place("b2"), Transition("t4")): return 1
+      case(Place("c0"), Transition("t3")): return 1
+
+      case(Place("c2"), Transition("t4")): return 1
+      case(Place("c1"), Transition("t4")): return 1
       default: return 0
    }
    }
 
   return PetriNet(
-    places: [Place("b0"), Place("b1"), Place("b2")],
-    transitions: [Transition("t1"), Transition("t2"), Transition("t3"), Transition("t4"), Transition("t5")],
+    places: [Place("b0"), Place("c0"),Place("b1"), Place("c1"), Place("b2"), Place("c2")],
+    transitions: [Transition("t1"), Transition("t2"), Transition("t3"), Transition("t4")],
     pre: pre,
     post: post)
 
@@ -41,7 +47,12 @@ public func createCounterModel() -> PetriNet {
 public func createCounterInitialMarking() -> Marking {
   func initialMarking(_ place: Place) -> Nat {
   switch place{
-    case Place("b0"): return 1
+    case Place("b0"): return 0
+    case Place("p0"): return 1
+    case Place("b1"): return 0
+    case Place("p1"): return 1
+    case Place("b2"): return 0
+    case Place("p2"): return 1
     default : return 0
     }
   }
