8e599c5a1a2229ece7da6b88bc64f5ea2a7a4faa
Kenania <kenaniadauphin@gmail.com>
2018-10-15 19:34:54 +0200

    tp1

--- tp-01/Sources/Sema/main.swift
+++ tp-01/Sources/Sema/main.swift
@@ -42,7 +42,6 @@ print()
 // Obtain the marking `m2` by firing `t1` from the initial marking.
 if let m2 = petrinet.fire(Transition("t1"), from: initialMarking) {
   // Note the use of a optional binding (`if let ...`), which means `m2` can't be `nil` here.
-
   // Print the marking `m2`
   print("Firing 't1' from the initial marking produces the following marking:")
   petrinet.print(marking: m2)
@@ -58,7 +57,6 @@ do {
     transitions: [Transition("t1")],  // `T = { t1 }`
     pre        : { _, _ in 1 },          // `pre(p1, t1) = 1`
     post       : { _, _ in 0 })          // `post(p1, t1) = 0`.
-
   // Compute the new marking after firing t1 from [p1 → 1].
   func m0(_ place: Place) -> Nat {
     return place == Place("p1") ? 1 : 0
--- tp-01/Sources/SemaLib/CounterModel.swift
+++ tp-01/Sources/SemaLib/CounterModel.swift
@@ -1,11 +1,60 @@
+// Initialisation des preconditions
+private func pre(p: Place, t: Transition) -> Nat {
+  switch (p, t) {
+  case (Place("s"), Transition("t3")): return 4
+  case (Place("b0"), Transition("t0")): return 1
+  case (Place("b1"), Transition("t1")): return 1
+  case (Place("b2"), Transition("t2")): return 1
+  default: return 0
+  }
+}
+// Initialisation des postconditions
+private func post(p: Place, t: Transition) -> Nat {
+  switch (p, t) {
+  case (Place("s"), Transition("t0")): return 1
+  case (Place("b0"), Transition("t1")): return 1
+  case (Place("b0"), Transition("t2")): return 1
+  case (Place("b0"), Transition("t3")): return 1
+  case (Place("b1"), Transition("t2")): return 1
+  case (Place("b1"), Transition("t3")): return 1
+  case (Place("b2"), Transition("t3")): return 1
+  default: return 0
+  }
+}
+
+let petrinet = PetriNet(
+  places      : [Place("s"), Place("b0"), Place("b1"), Place("b2")],
+  transitions : [Transition("t0"), Transition("t1"), Transition("t2"), Transition("t3")],
+  pre         : pre,
+  post        : post)
+
 /// This function creates the model of a binary counter on three bits.
 public func createCounterModel() -> PetriNet {
   // Write your code here.
-  return PetriNet(places: [], transitions: [], pre: { _, _ in 0 }, post: { _, _ in 0 })
+  return PetriNet(places: [Place("s"), Place("b0"), Place("b1"), Place("b2")], transitions: [Transition("t0"), Transition("t1"), Transition("t2"), Transition("t3")], pre: { _, _ in 0 }, post: { _, _ in 0 })
 }
 
 /// This function returns the initial marking corresponding to the model of your binary counter.
+/*
+switch place {
+case Place("p0"): return 1
+default: return 0
+}
+*/
 public func createCounterInitialMarking() -> Marking {
   // Write your code here.
-  return { _ in 0 }
+  return  {
+
+  switch ($0){
+  case (Place("s")): return 4
+  default: return 0
+  }
+  }
 }
+
+
+
+/*
+  private func initialMarking(_ place: Place) -> Nat {
+
+  }*/
