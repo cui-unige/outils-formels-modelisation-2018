176947538902552a6f01b03bb163598ab338859b
Marko Smiljkovic <Ikam6@MacBook-Pro-de-Marko.local>
2018-10-19 14:03:32 +0200

    Correction exo-04

--- ex-04/Sources/Properties/Properties.swift
+++ ex-04/Sources/Properties/Properties.swift
@@ -10,20 +10,45 @@ func analyze<PlaceSet>(
     return
   }
   print("There are \(states.count) state(s) accessible from the initial marking.")
+  print(type(of: states))
 
   // What is the bound of the model?
-  let bound = 0 // TODO
+  // mode fonctionnel :
+  // utiliser la fonction map() --> retourne un tableau
+  // la fonction max() --> retourne un optionel
+  // on pourrait mettre place au lieu de _ // _ permet d'ignorer la variable.
+  let bound = states.map({ state in state.marking.map({ (_, token) in token }).max()! })
+  // mode iteravtive :
+  // var bound: UInt = 0
+  // for state in states {
+  //   for (_, token) in state.marking {
+  //     bound = max(bound, token)
+  //   }
+  // }
+  print(type(of: bound))
   print("The model is \(bound)-bounded.")
 
   // Is the model L3-live (i.e. "vivant")?
-  let isL3 = false // TODO
+  let isL3 = model.transitions.allSatisfy({ transition in
+    states.allSatisfy({ m in
+    m.contains(where: { transition.isFireable(from: $0.marking) })
+   })
+  })
   print("The model is\(!isL3 ? " not" : "") L3-live.")
 
   // Is the model L1-live (i.e. "quasi-vivant")?
-  let isL1 = false // TODO
+  let isL1 = model.transitions.allSatisfy({ transition in
+    states.contains(where: { transition.isFireable(from: $0.marking) })
+  })
   print("The model is\(!isL1 ? " not" : "") L1-live.")
 
   // Is the model dead?
-  let isDead = false // TODO
+  let isDead = states.contains(where: { state in state.successors.isEmpty })
+  // let isDead2 = states.allSatisfy({ state in
+  //   model.transitions.contains(where: { $0.isFireable(from: state.marking) })
+  // })
   print("The model is\(!isDead ? " not" : "") dead.")
+
+  let deadState = states.first(where: { state in state.successors.isEmpty })!
+  print(deadState.marking)
 }
