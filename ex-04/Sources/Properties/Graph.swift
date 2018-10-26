759abb39d2f6dd86864b098a3c5a91b3f357a088
Luca <Luca.Rizzo@etu.unige.com>
2018-10-26 12:41:28 +0200

    yes

--- ex-04/Sources/Properties/Graph.swift
+++ ex-04/Sources/Properties/Graph.swift
@@ -41,24 +41,24 @@ func computeGraph<Net>(of petrinet: Net, from initialMarking: Net.MarkingType) -
 {
   let root = Node<Net>(marking: initialMarking)
   var created = [root]
-  var unprocessed: [(Node<Net>, [Node<Net>])] = [(root, [])]
+  var unprocessed: [(Node<Net>, [Node<Net>])] = [(root, [])] // les visités
 
-  while let (node, predecessors) = unprocessed.popLast() {
-    for transition in petrinet.transitions {
-      guard let nextMarking = transition.fire(from: node.marking)
-        else { continue }
-      if let successor = created.first(where : { other in other.marking == nextMarking }) {
-        node.successors[transition] = successor
-      } else if predecessors.contains(where: { other in nextMarking > other.marking }) {
-        return nil
-      } else {
-        let successor = Node<Net>(marking: nextMarking)
-        created.append(successor)
-        unprocessed.append((successor, predecessors + [node]))
-        node.successors[transition] = successor
+  while let (node, predecessors) = unprocessed.popLast() { // on sort le visité pour l'analyser
+    for transition in petrinet.transitions { // on regarde les transition du visité
+      guard let nextMarking = transition.fire(from: node.marking) // on regarde si un maquage depuis le visité existe
+        else { continue } // sinon : nil , on continue            // on crée également le nouveau marquage
+      if let successor = created.first(where : { other in other.marking == nextMarking }) { // on prend le premier de liste créé on le compare entre le marquage initiale et le nouveau
+        node.successors[transition] = successor// si oui on a trouver un successeur  sinon ce n'est pas un successeur , si oui on l'ajoute à la liste des successeurs
+      } else if predecessors.contains(where: { other in nextMarking > other.marking }) { // si le nouveou marquage est plus grand que l'encien d'un appartenant à la liste de prédecesseur alors on l'ajoute pas
+        return nil // pas plus grand qu'un predecessors alors on l'ajoute pas
+      } else { // evaluation terminer pour un état on passe au prochains mais en répertoriant nos découvertes
+        let successor = Node<Net>(marking: nextMarking) // on créer un graphe successor pour l'ajouter au graphe le marquage initial devient nextmarking
+        created.append(successor)// on ajoute notre nouveau graphe au graphe successor
+        unprocessed.append((successor, predecessors + [node])) // on ajoute notre successor au noeud visité pour le prochain pas
+        node.successors[transition] = successor // on ajoute à l'index des successor ,associer à notre transition le successeur
       }
     }
   }
 
-  return root
+  return root // en retournant la racine on retourne tout le graphe procédé itératif implique dfs recherche en profondeur
 }
--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -7,6 +7,32 @@ extension PetriNet {
   public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
     // TODO: Replace or modify this code with your own implementation.
     let root = MarkingNode(marking: initialMarking)
+//****************** my test **************************************
+var created = [root]
+  var unprocessed: [(MarkingNode<Place>, [MarkingNode<Place>])] = [(root, [])] // les visités
+
+  while let (node, predecessors) = unprocessed.popLast() { // on sort le visité pour l'analyser
+    for transition in Petrinet.transitions { // on regarde les transition du visité
+      guard let nextMarking = transition.fire(from: node.marking) // on regarde si un maquage depuis le visité existe
+        else { continue } // sinon : nil , on continue            // on crée également le nouveau marquage
+      if let successor = created.first(where : { other in other.marking == nextMarking }) { // on prend le premier de liste créé on le compare entre le marquage initiale et le nouveau
+        node.successors[transition] = successor// si oui on a trouver un successeur  sinon ce n'est pas un successeur , si oui on l'ajoute à la liste des successeurs
+      } else if predecessors.contains(where: { other in nextMarking > other.marking }) { // si le nouveou marquage est plus grand que l'encien d'un appartenant à la liste de prédecesseur alors on l'ajoute pas
+        return nil // pas plus grand qu'un predecessors alors on l'ajoute pas
+      } else { // evaluation terminer pour un état on passe au prochains mais en répertoriant nos découvertes
+        let successor = MarkingNode<Place>(marking: nextMarking) // on créer un graphe successor pour l'ajouter au graphe le marquage initial devient nextmarking
+        created.append(successor)// on ajoute notre nouveau graphe au graphe successor
+        unprocessed.append((successor, predecessors + [node])) // on ajoute notre successor au noeud visité pour le prochain pas
+        node.successors[transition] = successor // on ajoute à l'index des successor ,associer à notre transition le successeur
+      }
+    }
+  }
+
+// en retournant la racine on retourne tout le graphe procédé itératif implique dfs recherche en profondeur
+
+
+
+//******************************************************************
     return root
   }
 
