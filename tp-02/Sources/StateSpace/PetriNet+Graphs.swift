ecb4fcb0deaf11082ff6663763604e5411ae4758
Roxane <roxane.burri@etu.unige.ch>
2018-10-28 11:59:10 +0100

    tp2

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -6,36 +6,26 @@ extension PetriNet {
   /// the root of the marking graph. If the model isunbounded, the function returns nil.
   public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
 
-    let root = MarkingNode(marking: initialMarking)
-
-    var  rootTable = [root]     //definition d'un tableau avec les éléments de root
-    //de makeIterator()
-    var unprocessed : [(MarkingNode<Place>,[MarkingNode<Place>])] = [(root,[])]
-    //var processed : [(MarkingNode<Place>,[MarkingNode<Place>])] = []
-    //Pas utilisée dans le programme
-
-    while let (node, predecessor) = unprocessed.popLast(){ //tant qu'il y a des noeuds
-      for transition in transitions{ //parcours toutes les transitions
-          //use guard
-        guard let nextnode = transition.fire(from : node.marking) //si le noeud suivant est égale au tirage de la transition
-        else {
-        continue //si la condition est vrai on continu la boucle
-    }
-        if let successor = rootTable.first(where: {element in element.marking == nextnode})  { //la fonction first retourne le premier element qui est egale a nextnode
-          node.successors[transition] = successor
+      let root = MarkingNode(marking: initialMarking)
+      var created = [root]
+      var unprocessed : [(MarkingNode<Place>,[MarkingNode<Place>])] = [(root,[])]
+      while let (node, predecessors) = unprocessed.popLast(){
+          for transition in transitions{
+              guard let nextmarking = transition.fire(from : node.marking)
+              else {continue}
+              if let successor = created.first(where: {other in other.marking == nextmarking})  {
+                  node.successors[transition] = successor
+              }else if predecessors.contains(where : {other in other.marking < nextmarking}) {
+                  return  nil
+              }else{
+                  let successor = MarkingNode(marking: nextmarking)
+                  created.append(successor)
+                  unprocessed.append((successor, predecessors + [node]))
+                  node.successors[transition] = successor
+              }
+          }
       }
-      else if predecessor.contains(where : {element in element.marking < nextnode}) { //contain retourne la position de l'élément qui est inférieur a nextnode
-            //si ce noeud est borné alors envoyer nul
-          return  nil
-        }else{
-           let successor = MarkingNode(marking: nextnode)
-           rootTable.append(successor) //remplir le taleau avec les noeuds
-           unprocessed.append((successor, predecessor + [node]))
-           node.successors[transition] = successor
-        }
-      }
-    }
-    return root
+      return root
   }
 
   /// Computes the coverability graph of this Petri net, starting from the given marking.
@@ -45,42 +35,38 @@ extension PetriNet {
   public func computeCoverabilityGraph(from initialMarking: Marking<Place, Int>)
     -> CoverabilityNode<Place>?
   {
-    let root = CoverabilityNode(marking: extend(initialMarking)) //extend permet de rajouter initialMarking a la fin de root
-        //même principe qu'au dessus
-    var rootTable = [root]
-var unprocessed : [(CoverabilityNode<Place>,[CoverabilityNode<Place>])] = [(root,[])]
-while let (node, predecessor) = unprocessed.popLast(){
-  for transition in transitions{
-    guard var nextnode = transition.fire(from : node.marking)
-    else {
-    continue
-}
-        if let Unbounded = predecessor.first(where: {element in element.marking < nextnode})  {
-          for place in Place.allCases {
-            if   Unbounded.marking[place] < nextnode[place]{
-              nextnode[place] = .omega
+      let root = CoverabilityNode(marking: extend(initialMarking))
+      var created = [root]
+      var unprocessed : [(CoverabilityNode<Place>,[CoverabilityNode<Place>])] = [(root,[])]
+      while let (node, predecessors) = unprocessed.popLast(){
+          for transition in transitions{
+              guard var nextmarking = transition.fire(from : node.marking)
+              else {continue}
+              if let greatSuccessor = predecessors.first(where: {other in other.marking < nextmarking})  {
+                  for place in Place.allCases {
+                      if   greatSuccessor.marking[place] < nextmarking[place]{
+                          nextmarking[place] = .omega
+                      }
+                  }
               }
-            }
-        }
-        if node.marking < nextnode{
-          for place in Place.allCases {
-            if   node.marking[place] < nextnode[place]{
-              nextnode[place] = .omega
+              if node.marking < nextmarking{
+                  for place in Place.allCases {
+                      if   node.marking[place] < nextmarking[place]{
+                          nextmarking[place] = .omega
+                      }
+                  }
               }
-            }
-        }
-
-    if let successor = rootTable.first(where: {element in element.marking == nextnode})  {
-      node.successors[transition] = successor
-    }else{
-       let successor = CoverabilityNode(marking: nextnode)
-       rootTable.append(successor)
-       unprocessed.append((successor, predecessor + [node]))
-       node.successors[transition] = successor
-    }
-  }
-}
-    return root
+              if let successor = created.first(where: {other in other.marking == nextmarking})  {
+                  node.successors[transition] = successor
+              }else{
+                  let successor = CoverabilityNode(marking: nextmarking)
+                  created.append(successor)
+                  unprocessed.append((successor, predecessors + [node]))
+                  node.successors[transition] = successor
+              }
+          }
+      }
+      return root
   }
 
   /// Converts a regular marking into a marking with extended integers.
