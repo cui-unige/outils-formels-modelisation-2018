3add7d94df100b1931fb1aa2d918b16e6509515f
Rose <rose@pc86.home>
2018-10-28 15:53:45 +0100

    ajout_exo1

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -5,9 +5,26 @@ extension PetriNet {
   /// This method computes the marking graph of the Petri net, assuming it is bounded, and returns
   /// the root of the marking graph. If the model isunbounded, the function returns nil.
   public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
-    // TODO: Replace or modify this code with your own implementation.
-    let root = MarkingNode(marking: initialMarking)
-    return root
+          let root = MarkingNode(marking: initialMarking)
+          var a_creer = [root]
+          var graphe_en_cour : [(MarkingNode<Place>,[MarkingNode<Place>])] = [(root,[])]
+          while let (node, predecessors) = graphe_en_cour.popLast(){
+              for t in transitions{
+                  if let prochaine_marquage = t.fire(from : node.marking){
+                    if predecessors.contains(where : {other in other.marking < prochaine_marquage}) {
+                     return  nil
+                    }else if let successor = a_creer.first(where: {other in other.marking == prochaine_marquage})  {
+                      node.successors[t] = successor
+                    }else{
+                       let successor = MarkingNode(marking: prochaine_marquage)
+                       a_creer.append(successor)
+                       graphe_en_cour.append((successor, predecessors + [node]))
+                       node.successors[t] = successor
+                    }
+                  }
+                  }
+                }
+                return root
   }
 
   /// Computes the coverability graph of this Petri net, starting from the given marking.
