f8af6fba8c67d8e241af5fa95fdec94267832c8e
ikam6 <Ikam6@macbook-pro-de-marko.home>
2018-10-29 20:17:48 +0100

    rendu TP2

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -11,19 +11,29 @@ extension PetriNet {
 
     // inspiration de Graph.swift dans ex-04 :
     // Voir aussi l'algorthme dans la theorie (RdPVerifProp.pdf)
+
+    // initialisation des racines pour marquage initial et
+    // des variables created et unprocessed pour travailelr sur les racines :
     let root = MarkingNode(marking: initialMarking)
     var created = [root]
     var unprocessed: [(MarkingNode<Place>, [MarkingNode<Place>])] = [(root, [])]
 
+    // Tant qu'il y a des elements "non-travailles" (unprocessed)
+    // pour toutes transitions verifier si elle est tirable et mettre dans nextMarking
     while let (node, predecessors) = unprocessed.popLast() {
       for transition in transitions {
         guard let nextMarking = transition.fire(from: node.marking)
         else { continue }
+        // si nouveau marquage deja existant, mettre comme successor dans noeud
         if let successor = created.first(where : { other in other.marking == nextMarking }) {
           node.successors[transition] = successor
-        } else if predecessors.contains(where: { other in nextMarking > other.marking }) {
+        }
+        // sinon  si nouveau marquage plus grand que les autres, marquage non-borne donc nil
+        else if predecessors.contains(where: { other in other.marking < nextMarking }) {
           return nil
-        } else {
+        }
+        // sinon on creer le noeud suivant avec le nouveau marquage
+        else {
           let successor = MarkingNode(marking: nextMarking)
           created.append(successor)
           unprocessed.append((successor, predecessors + [node]))
@@ -31,7 +41,6 @@ extension PetriNet {
         }
       }
     }
-
     return root
   }
 
@@ -40,12 +49,58 @@ extension PetriNet {
   /// This method computes the coverability graph of the Petri net, and returns its root. Note that
   /// if the model's bound, the coverability graph is actually equivalent to the marking one.
   public func computeCoverabilityGraph(from initialMarking: Marking<Place, Int>)
-    -> CoverabilityNode<Place>?
+   -> CoverabilityNode<Place>?
   {
     // TODO: Replace or modify this code with your own implementation.
+
+    // inspiration de Graph.swift dans ex-04 :
+    // Voir aussi l'algorthme dans la theorie (RdPVerifProp.pdf)
+
+    // initialisation des racines pour marquage initial et
+    // des variables created et unprocessed pour travailelr sur les racines :
     let root = CoverabilityNode(marking: extend(initialMarking))
-    return root
-  }
+    var created = [root]
+    var unprocessed: [(CoverabilityNode<Place>, [CoverabilityNode<Place>])] = [(root, [])]
+
+    // Tant qu'il y a des elements "non-travailles" (unprocessed)
+    // pour toutes transitions verifier si elle est tirable et mettre dans nextMarking
+    while let (node, predecessors) = unprocessed.popLast() {
+      for transition in transitions {
+        guard var nextMarking = transition.fire(from: node.marking) //var et non let sinon marche pas
+        else { continue }
+        // si un nouveau marquage plus grand que les autres on regarde chaque place de predecessor
+        // et si la valeur de place du marquage successor est plus petit que valeur du nouveau
+        // alors on modifie la place avec omega
+        if let successor = predecessors.first(where: { other in other.marking < nextMarking }) {
+          for  place in Place.allCases {
+            if successor.marking[place] < nextMarking[place] {
+              nextMarking[place] = .omega
+            }
+          }
+        }
+        // ici on verifie pour le noeud comme avant
+        if node.marking < nextMarking {
+          for place in Place.allCases {
+            if node.marking[place] < nextMarking[place] {
+              nextMarking[place] = .omega
+            }
+          }
+        }
+        // si nouveau marquage deja existant, mettre comme successor dans noeud
+        if let successor = created.first (where : { other in other.marking == nextMarking }) {
+          node.successors[transition] = successor
+        }
+        // sinon on creer le noeud suivant avec le nouveau marquage
+        else {
+          let successor = CoverabilityNode(marking: nextMarking)
+          created.append(successor)
+          unprocessed.append((successor, predecessors + [node]))
+          node.successors[transition] = successor
+        }
+      }
+    }
+  return root
+}
 
   /// Converts a regular marking into a marking with extended integers.
   private func extend(_ marking: Marking<Place, Int>) -> Marking<Place, ExtendedInt> {
