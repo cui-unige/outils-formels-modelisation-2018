e83f702762015a5cb056d17a604c03974e5fdda3
Jean-Luc STOUPY <jean-luc.stoupy@etu.unige.ch>
2018-10-29 18:40:04 +0100

    TP2

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -39,13 +39,43 @@ extension PetriNet {
   ///
   /// This method computes the coverability graph of the Petri net, and returns its root. Note that
   /// if the model's bound, the coverability graph is actually equivalent to the marking one.
-  public func computeCoverabilityGraph(from initialMarking: Marking<Place, Int>)
-    -> CoverabilityNode<Place>?
-  {
-    // TODO: Replace or modify this code with your own implementation.
-    let root = CoverabilityNode(marking: extend(initialMarking))
-    return root
-  }
+    public func computeCoverabilityGraph(from initialMarking: Marking<Place, Int>)
+        -> CoverabilityNode<Place>?
+    {
+        // TODO: Replace or modify this code with your own implementation.
+        let root = CoverabilityNode(marking: extend(initialMarking))
+        var created = [root]
+        var unprocessed : [(CoverabilityNode<Place>,[CoverabilityNode<Place>])] = [(root,[])]
+        while let (node, predecessors) = unprocessed.popLast(){
+            for transition in transitions{
+                guard var nextmarking = transition.fire(from : node.marking)
+                    else {continue}
+                if let greatSuccessor = predecessors.first(where: {other in other.marking < nextmarking})  {
+                    for place in Place.allCases {
+                        if   greatSuccessor.marking[place] < nextmarking[place]{
+                            nextmarking[place] = .omega
+                        }
+                    }
+                }
+                if node.marking < nextmarking{
+                    for place in Place.allCases {
+                        if   node.marking[place] < nextmarking[place]{
+                            nextmarking[place] = .omega
+                        }
+                    }
+                }
+                if let successor = created.first(where: {other in other.marking == nextmarking})  {
+                    node.successors[transition] = successor
+                }else{
+                    let successor = CoverabilityNode(marking: nextmarking)
+                    created.append(successor)
+                    unprocessed.append((successor, predecessors + [node]))
+                    node.successors[transition] = successor
+                }
+            }
+        }
+        return root
+    }
 
   /// Converts a regular marking into a marking with extended integers.
   private func extend(_ marking: Marking<Place, Int>) -> Marking<Place, ExtendedInt> {
