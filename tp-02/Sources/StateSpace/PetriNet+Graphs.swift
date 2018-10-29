90cdb6082a6b03ce7e9ce0953bb394dda7acb680
Sasha Poirier <sasha.poirier@gmail.com>
2018-10-29 19:23:57 +0100

    TP 2 guess is final

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -4,46 +4,45 @@ extension PetriNet {
   ///
   /// This method computes the marking graph of the Petri net, assuming it is bounded, and returns
   /// the root of the marking graph. If the model isunbounded, the function returns nil.
-  public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
-    // TODO: Replace or modify this code with your own implementation.
+  public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>?
+  {
     let root = MarkingNode(marking: initialMarking)
     var created = [root]
     var unprocessed: [(MarkingNode<Place>, [MarkingNode<Place>])] = [(root, [])]
 
     while let (node, predecessors) = unprocessed.popLast() // on sort le visité pour l'analyser
     {
+      //for all transitions
       for transition in self.transitions
       {
+        //if the transition to reach a new marking is fireable
         if let nextMarking = transition.fire(from: node.marking)
         {
-          // on prend le premier de liste créé on le compare entre le marquage initiale et le nouveau
+          //take the first from the list and compare it to the initial marking and the new marking
           if let successor = created.first(where : {element in element.marking == nextMarking})
           {
-            // si oui on a trouver un successeur  sinon ce n'est pas un successeur,
-            // si oui on l'ajoute à la liste des successeurs
+            //if yes a successor has been found, and it is added to the lsit
             node.successors[transition] = successor
           }
 
-          // si le nouveou marquage est plus grand que l'encien d'un appartenant
-          //à la liste de prédecesseur alors on l'ajoute pas
+          //if the marking is greater than one already on the list we don't add it
           else if predecessors.contains(where: {element in nextMarking > element.marking})
           {
-            return nil // pas plus grand qu'un predecessors alors on l'ajoute pas
+            return nil
           }
 
-          // evaluation terminer pour un état on passe au prochains mais en répertoriant nos découvertes
           else
           {
-            // on créer un graphe successor pour l'ajouter au graphe le marquage initial devient nextmarking
+            //create a new Marking for the successor
             let successor = MarkingNode<Place>(marking: nextMarking)
 
-            // on ajoute notre nouveau graphe au graphe successor
+            //add the successor to the created graph
             created.append(successor)
 
-            // on ajoute notre successor au noeud visité pour le prochain pas
+            //add the new marking to be analysed
             unprocessed.append((successor, predecessors + [node]))
 
-            // on ajoute à l'index des successor ,associer à notre transition le successeur
+            //add the successor and associate it to the transition that resulted in said successor
             node.successors[transition] = successor
           }
         }
@@ -58,48 +57,53 @@ extension PetriNet {
   /// if the model's bound, the coverability graph is actually equivalent to the marking one.
   public func computeCoverabilityGraph(from initialMarking: Marking<Place, Int>) -> CoverabilityNode<Place>?
   {
-    // TODO: Replace or modify this code with your own implementation.
     let root = CoverabilityNode(marking: extend(initialMarking))
     var created = [root]
     var unprocessed : [(CoverabilityNode<Place>, [CoverabilityNode<Place>])] = [(root, [])]
 
-    while let (node, predecessor) = unprocessed.popLast()
+    while let (node, predecessors) = unprocessed.popLast()
     {
       for transition in self.transitions
       {
+        //if the transition to reach the next marking is fireable
         if var nextMarking = transition.fire (from: node.marking)
         {
-          if let unbounded = predecessor.first(where: {element in element.marking < nextMarking})
+          //check to make sure that the marking is less than the new Marking
+          if let F = predecessors.first(where: {element in element.marking < nextMarking})
           {
             for place in Place.allCases
             {
-              if unbounded.marking[place] < nextMarking[place]
+              if F.marking[place] < nextMarking[place]
               {
+                //if the place for the new marking is greater than in the marking, it is an Omega
                 nextMarking[place] = .omega
               }
             }
           }
 
+          //check to make sure that the previous marking is less than the new marking
           if node.marking < nextMarking
           {
             for place in Place.allCases
             {
               if (node.marking[place] < nextMarking[place])
               {
+                //if the place for the new marking is greater than in the previous marking, it is an Omega
                 nextMarking[place] = .omega
               }
             }
           }
-
+          
+          //same idea as previous method
           if let successor = created.first(where: {element in element.marking == nextMarking})
           {
             node.successors[transition] = successor
           }
           else
           {
-            let successor = CoverabilityNode(marking: nextMarking)
+            let successor = CoverabilityNode(marking: nextMarking)  //only difference from previous method is CovNode type here
             created.append(successor)
-            unprocessed.append((successor, predecessor + [node]))
+            unprocessed.append((successor, predecessors + [node]))
             node.successors[transition] = successor
           }
         }
