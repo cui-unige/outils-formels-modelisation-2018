cd98e69d227416b1c2663e8a0c70399118e553e8
Kenania <kenaniadauphin@gmail.com>
2018-10-29 21:37:11 +0100

    TP2

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -7,7 +7,41 @@ extension PetriNet {
   public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
     // TODO: Replace or modify this code with your own implementation.
     let root = MarkingNode(marking: initialMarking)
-    return root
+    var explored = [root] // Tableau des noeuds (places) explorées
+    var inProcess: [(MarkingNode<Place>, [MarkingNode<Place>])] = [(root, [])] // Contient des places et leurs prédécesseurs
+
+    while let (node, predecessors) = inProcess.popLast() // On réupère le dernier noeud et ses prédécesseurs
+   {
+     for transition in self.transitions // On parcourt chacune de ses transitions
+     {
+       if let nextMarking = transition.fire(from: node.marking) // Pour chaque transition, on vérifie si elle est tirable et si oui, on récupère le nouveau marquage
+       {
+         // On compare le nouveau marquage avec le premier
+         if let successor = explored.first(where : {element in element.marking == nextMarking})
+         {
+           // Si il est égal, on ajoute les nouvelles transitions à la liste des successeurs
+           node.successors[transition] = successor
+         }
+         else if predecessors.contains(where: {element in nextMarking > element.marking})
+         {
+           // Si le nouveau marquage est plus grand on retourne nil
+           return nil
+         }
+         else
+         {
+           // Sinon, on considère notre successeur comme un nouveau graph
+           let successor = MarkingNode<Place>(marking: nextMarking)
+           // on ajoute notre nouveau graphe au tableau des explorés
+           explored.append(successor)
+           // on ajoute notre successor aux noeuds "inProcess" pour qu'il soit exploré
+           inProcess.append((successor, predecessors + [node]))
+           // on l'ajoute à l'index des successors du noeud en cours d'exploration
+           node.successors[transition] = successor
+         }
+       }
+     }
+   }
+   return root
   }
 
   /// Computes the coverability graph of this Petri net, starting from the given marking.
@@ -19,6 +53,50 @@ extension PetriNet {
   {
     // TODO: Replace or modify this code with your own implementation.
     let root = CoverabilityNode(marking: extend(initialMarking))
+    // Même principe que plus haut
+    var created = [root]
+    var unprocessed : [(CoverabilityNode<Place>, [CoverabilityNode<Place>])] = [(root, [])]
+
+    while let (node, predecessor) = unprocessed.popLast()
+    {
+      for transition in self.transitions
+      {
+        if var nextMarking = transition.fire (from: node.marking)
+        {
+          if let unbounded = predecessor.first(where: {element in element.marking < nextMarking})
+          {
+            for place in Place.allCases
+            {
+              if unbounded.marking[place] < nextMarking[place]
+              {
+                nextMarking[place] = .omega
+              }
+            }
+          }
+          if node.marking < nextMarking
+          {
+            for place in Place.allCases
+            {
+              if (node.marking[place] < nextMarking[place])
+              {
+                nextMarking[place] = .omega
+              }
+            }
+          }
+          if let successor = created.first(where: {element in element.marking == nextMarking})
+          {
+            node.successors[transition] = successor
+          }
+          else
+          {
+            let successor = CoverabilityNode(marking: nextMarking)
+            created.append(successor)
+            unprocessed.append((successor, predecessor + [node]))
+            node.successors[transition] = successor
+          }
+        }
+      }
+    }
     return root
   }
 
