89257d06ed4c3e1488a2d3c686172d0bdc57ff52
Christian <Christian.Ney@etu.unige.ch>
2018-10-29 22:39:47 +0100

    test

--- tp-01/Sources/SemaLib/CounterModel.swift
+++ tp-01/Sources/SemaLib/CounterModel.swift
@@ -1,11 +1,29 @@
 /// This function creates the model of a binary counter on three bits.
 public func createCounterModel() -> PetriNet {
-  // Write your code here.
-  return PetriNet(places: [], transitions: [], pre: { _, _ in 0 }, post: { _, _ in 0 })
+  var preCond = {
+    p : Place, t : Transition ->
+    Nat in
+      var value = 0
+      if ((p == b0 && t != t0) || (p == b1 && (t == t2 || t == t3)) || (p == b2 && t == t3)) {
+        value = 1
+      }
+      return value
+  }                                                                             /// function pre of the generated PetriNet model
+
+  var postCond = {
+    p : Place, t : Transition ->
+    Nat in
+      var value = 0
+      if ((p == b0 && t == t0) || (p == b1 && t == t1) || (p == b2 && t == t2)) {
+        value = 1
+      }
+      return value                                                              /// function post of the generated PetriNet model
+  }
+
+  return PetriNet(places: [p0, p1, p2], transitions: [t0, t1, t2, t3], pre: preCond, post: postCond)     /// generating and returning the generated PetriNet model
 }
 
 /// This function returns the initial marking corresponding to the model of your binary counter.
 public func createCounterInitialMarking() -> Marking {
-  // Write your code here.
-  return { _ in 0 }
+  return { place : Place -> Nat in return 0  }
 }
