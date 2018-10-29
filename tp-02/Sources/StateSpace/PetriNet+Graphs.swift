915f7ea76ba647c2b166580509c85a2db816794b
Etra <etra.alamadani@etu.unige.ch>
2018-10-29 18:09:54 +0100

    vertion final tp2

--- tp-02/Sources/StateSpace/PetriNet+Graphs.swift
+++ tp-02/Sources/StateSpace/PetriNet+Graphs.swift
@@ -7,26 +7,27 @@ extension PetriNet {
   public func computeMarkingGraph(from initialMarking: Marking<Place, Int>) -> MarkingNode<Place>? {
     // TODO: Replace or modify this code with your own implementation.
 
-       let root = MarkingNode(marking: initialMarking) //On initialise root avec le marking initial, renvoie un marquage et ses successeurs
-       var created = [root] //Vecteur contenant les noeuds différents créés
-       var unprocessed : [(MarkingNode<Place>,[MarkingNode<Place>])] = [(root,[])] //Vecteur avec les éléments qu'on doit regarder, composé d'abord du noeud puis du prédécesseur
-       while let (node, predecessors) = unprocessed.popLast(){ //Tant qu'il y a encore des éléments à regarder, on prend le dernier
-         for transition in transitions{ //On boucle sur toutes les transitions pour voir les marquages obtenus
-           guard let nextmarking = transition.fire(from : node.marking) //Le prochain marquage est celui obtenu après le tirage de la transition
-           else {continue}
-           if let successor = created.first(where: {other in other.marking == nextmarking})  { //Si on a déjà vu le prochain marquage, s'il existe déjà, le first prends le premier élément qui vérifie la condition
-              node.successors[transition] = successor//le successeur dans le graph du noeud après la transition est successor, le noeud créé qui existe déjà, on ne le touche plus par la suite
+       let root = MarkingNode(marking: initialMarking) //declartion de root avec le markage intial
+       var created = [root] //variable danslaquelle on mit les noeuds
+       var unprocessed : [(MarkingNode<Place>,[MarkingNode<Place>])] = [(root,[])] //(variable unprocessed avec les élement que on que on doit parcourir
+       while let (node, predecessors) = unprocessed.popLast(){ //on choisi le dernier élément tant qu il existe encore des element à regarder
+         for transition in transitions{ //on parcoure toutes les transition pour voir les marquages qui sont franchissable
+           guard let nextmarking = transition.fire(from : node.marking) //on prends le marquage successor apres le tirage de transition
+           else {continue} //sinon on prends le prochain
+           if let successor = created.first(where: {other in other.marking == nextmarking})  { //sinon il prends le premeier qu il n'est pas était traiter
+              node.successors[transition] = successor//on gard le successeur
            }else if predecessors.contains(where : {other in other.marking < nextmarking}) { //Si sur le chemin il y a un marquage plus petit que nextmarking, c'est pas borné
              return  nil
-           }else{ //Si on a pas encore vus le marquage
-              let successor = MarkingNode(marking: nextmarking) //Création du noeud nextmarking et ses successeurs
-              created.append(successor) //On ajoute le marking dans les marking créés
-              unprocessed.append((successor, predecessors + [node])) //On le met dans unprocessed pour le faire après, le noeud devient le prédessesseur et successor est le noeud
-              node.successors[transition] = successor //le successeur dans le graph du noeud après la transition est le noeud créé
+           }else{ //(sinon on condiere que c'est un nouveau node
+             //on va garder ces noueds et ses markages
+              let successor = MarkingNode(marking: nextmarking)
+              created.append(successor)
+              unprocessed.append((successor, predecessors + [node]))
+              node.successors[transition] = successor
            }
          }
        }
-         return root //On retourne root, donc le marking graph puisqu'il contient les successeurs
+         return root //on renvoie les racines
   }
 
   /// Computes the coverability graph of this Petri net, starting from the given marking.
@@ -38,13 +39,13 @@ extension PetriNet {
   {
     // TODO: Replace or modify this code with your own implementation.
 
-     let root = CoverabilityNode(marking: extend(initialMarking)) //On initialise root avec le marking initial, renvoie un marquage et ses successeurs
+     let root = CoverabilityNode(marking: extend(initialMarking)) //declartion de root avec le markage intial
 
-     var created = [root]  //Vecteur contenant les noeuds différents créés
-         var unprocessed : [(CoverabilityNode<Place>,[CoverabilityNode<Place>])] = [(root,[])] //Vecteur avec les éléments qu'on doit regarder, composé d'abord du noeud puis du prédécesseur
-         while let (node, predecessors) = unprocessed.popLast(){ //Tant qu'il y a encore des éléments à regarder, on prend le dernier
-           for transition in transitions{  //On boucle sur toutes les transitions pour voir les marquages obtenus
-             guard var nextmarking = transition.fire(from : node.marking) //Le prochain marquage est celui obtenu après le tirage de la transition
+     var created = [root]  //variable danslaquelle on mit les noeuds
+         var unprocessed : [(CoverabilityNode<Place>,[CoverabilityNode<Place>])] = [(root,[])] //variable unprocessed avec les élement que on que on doit parcourir
+         while let (node, predecessors) = unprocessed.popLast(){ //on choisi le dernier élément tant qu il existe encore des element à regarder
+           for transition in transitions{  //on parcoure toutes les transition
+             guard var nextmarking = transition.fire(from : node.marking) //on garde le marcage après le tirage de la transition
              else {continue}
                  if let predecessor = predecessors.first(where: {other in other.marking < nextmarking})  { //Si on trouve un ancien marquage plus petit, on prends le premier comme ceci et on le met dans predecessor
                    for place in Place.allCases { //On regarde toutes les places
@@ -53,21 +54,22 @@ extension PetriNet {
                        }
                      }
                  }
-                 if node.marking < nextmarking{  //Si le marquage du noeud qu'on regarde est plus petit que le prochain marquage qui va suivre (on le test car il n'a pas été testé dans les prédecesseurs)
-                   for place in Place.allCases { //On regarde toutes les places
-                     if node.marking[place] < nextmarking[place]{ //S'il y a moins de jetons dans la place du marquage courant que dans la place de nextmarking
-                       nextmarking[place] = .omega //On met oméga dans la place de nextmarking car ce n'est pas borné dans cette place
+                 if node.marking < nextmarking{  //Si le marquage du noeud qu'on regarde est plus petit que le prochain marquage qui va suivre "on le test car il n'a pas été testé dans les prédecesseurs"
+                   for place in Place.allCases { //parcourir de toutes les places
+                     if nextmarking[place] > node.marking[place]{ //on test s'il y a plus de jetons dans la place du prochaine markage que dans le courant
+                       nextmarking[place] = .omega //On met oméga dans la place
                        }
                      }
                  }
              if let successor = created.first(where: {other in other.marking == nextmarking})  { //Si on a déjà vu le prochain marquage, s'il existe déjà, on prend le premier tel marquage
                node.successors[transition] = successor //le successeur dans le graph du noeud après la transition est le noeud créé, on ne le touche plus par la suite, on l'a déjà visité
              }
-             else{ //Si il n'y a pas d'ancien marquage égal à celui-ci
-                let successor = CoverabilityNode(marking: nextmarking)  //Création du noeud nextmarking et ses successeurs
-                created.append(successor)  //On ajoute le marking dans les marking créés
-                unprocessed.append((successor, predecessors + [node])) //On le met dans unprocessed pour le faire après, le noeud devient le prédessesseur et successor est le noeud
-                node.successors[transition] = successor  //le successeur dans le graph du noeud après la transition est le noeud créé
+             else{ //(sinon on condiere que c'est un nouveau node
+               //on va garder ces noueds et ses markages
+                let successor = CoverabilityNode(marking: nextmarking)
+                created.append(successor)
+                unprocessed.append((successor, predecessors + [node]))
+                node.successors[transition] = successor
              }
            }
          }
