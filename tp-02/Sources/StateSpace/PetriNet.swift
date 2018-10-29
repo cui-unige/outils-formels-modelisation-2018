8984ffc3cc8ed80ea5a9cefbc7c6b5d7cb5f64d5
Arnaud <Arnaud.Savary@etu.unige.ch>
2018-10-29 18:37:09 +0100

    Toujours notre TP 2

--- tp-02/Sources/StateSpace/PetriNet.swift
+++ tp-02/Sources/StateSpace/PetriNet.swift
@@ -1,91 +1,81 @@
 public struct PetriNet<Place> where Place: CaseIterable & Hashable {
+    public init(transitions: Set<Transition<Place>>) {
+        self.transitions = transitions
+    }
 
-  public init(transitions: Set<Transition<Place>>) {
-    self.transitions = transitions
-  }
-
-  let transitions: Set<Transition<Place>>
-
+    let transitions: Set<Transition<Place>>
 }
 
 public struct Transition<Place>: Hashable where Place: CaseIterable & Hashable {
-
-  public init(pre: Set<Arc<Place>>, post: Set<Arc<Place>>) {
-    self.preconditions = pre
-    self.postconditions = post
-  }
-
-  let preconditions: Set<Arc<Place>>
-  let postconditions: Set<Arc<Place>>
-
-  public func isFireable(from marking: Marking<Place, Int>) -> Bool {
-    return preconditions.allSatisfy { arc in arc.label <= marking[arc.place] }
-  }
-
-  public func isFireable(from marking: Marking<Place, ExtendedInt>) -> Bool {
-    return preconditions.allSatisfy { arc in .concrete(arc.label) <= marking[arc.place] }
-  }
-
-  public func fire(from marking: Marking<Place, Int>) -> Marking<Place, Int>? {
-    guard self.isFireable(from: marking) else {
-      return nil
+    public init(pre: Set<Arc<Place>>, post: Set<Arc<Place>>) {
+        preconditions = pre
+        postconditions = post
     }
 
-    var result = marking
-    for arc in self.preconditions {
-      result[arc.place] -= arc.label
-    }
-    for arc in self.postconditions {
-      result[arc.place] += arc.label
-    }
+    let preconditions: Set<Arc<Place>>
+    let postconditions: Set<Arc<Place>>
 
-    return result
-  }
-
-  public func fire(from marking: Marking<Place, ExtendedInt>) -> Marking<Place, ExtendedInt>? {
-    guard self.isFireable(from: marking) else {
-      return nil
+    public func isFireable(from marking: Marking<Place, Int>) -> Bool {
+        return preconditions.allSatisfy { arc in arc.label <= marking[arc.place] }
     }
 
-    var result = marking
-    for arc in self.preconditions {
-      result[arc.place] -= .concrete(arc.label)
-    }
-    for arc in self.postconditions {
-      result[arc.place] += .concrete(arc.label)
+    public func isFireable(from marking: Marking<Place, ExtendedInt>) -> Bool {
+        return preconditions.allSatisfy { arc in .concrete(arc.label) <= marking[arc.place] }
     }
 
-    return result
-  }
+    public func fire(from marking: Marking<Place, Int>) -> Marking<Place, Int>? {
+        guard isFireable(from: marking) else {
+            return nil
+        }
 
+        var result = marking
+        for arc in preconditions {
+            result[arc.place] -= arc.label
+        }
+        for arc in postconditions {
+            result[arc.place] += arc.label
+        }
+
+        return result
+    }
+
+    public func fire(from marking: Marking<Place, ExtendedInt>) -> Marking<Place, ExtendedInt>? {
+        guard isFireable(from: marking) else {
+            return nil
+        }
+
+        var result = marking
+        for arc in preconditions {
+            result[arc.place] -= .concrete(arc.label)
+        }
+        for arc in postconditions {
+            result[arc.place] += .concrete(arc.label)
+        }
+
+        return result
+    }
 }
 
 extension Transition: CustomStringConvertible {
-
-  public var description: String {
-    let pre  = preconditions .map({ $0.description }).joined(separator: ", ")
-    let post = postconditions.map({ $0.description }).joined(separator: ", ")
-    return "\(pre) -> [] -> \(post)"
-  }
-
+    public var description: String {
+        let pre = preconditions.map({ $0.description }).joined(separator: ", ")
+        let post = postconditions.map({ $0.description }).joined(separator: ", ")
+        return "\(pre) -> [] -> \(post)"
+    }
 }
 
 public struct Arc<Place>: Hashable where Place: Hashable {
+    public init(place: Place, label: Int = 1) {
+        self.place = place
+        self.label = label
+    }
 
-  public init(place: Place, label: Int = 1) {
-    self.place = place
-    self.label = label
-  }
-
-  public let place: Place
-  public let label: Int
-
+    public let place: Place
+    public let label: Int
 }
 
 extension Arc: CustomStringConvertible {
-
-  public var description: String {
-    return "(\(place), \(label))"
-  }
-
+    public var description: String {
+        return "(\(place), \(label))"
+    }
 }
