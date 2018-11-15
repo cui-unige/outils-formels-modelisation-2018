333f5b0ff0e641161f629d80bef38e671c9230d2
Ella Kummer <ella.kummer@etu.unige.ch>
2018-11-15 18:59:49 +0100

    tp03

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,14 +1,29 @@
 /// This function creates the model of a natural divider.
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
   // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+	// InhibitorNet<DividerPlaceSet>  fait de places et transitions (voir inhibitor init)
+  return InhibitorNet(
+    places: Set(DividerPlaceSet.allCases), // on garde toutes les places (les mêmes)
+    transitions: [
+      InhibitorNet.Transition(
+        name: "add", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.sto: 1]),
+				// Refills the tokens of `opa`.
+      InhibitorNet.Transition(
+        name: "rfl", pre: [.ena: 1, .sto: 1], post: [.ena: 1, .opb: 1]), // (ne change pas)
+				// Activates the refilling of `opa`.
+      InhibitorNet.Transition(
+        name: "ch1", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1]), // (ne change pas)
+				// élément neutre (<-> final) de la division = 1
+      InhibitorNet.Transition(
+        name: "ch2", pre: [.ena: 1, .sto: .inhibitor], post: [.res: 1]),
+    ])
 }
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
   // Write your code here.
-  return [:]
+  return [.opa: opa, .opb: opb, .res: 0, .ena: 0, .sto: 0]
 }
 
 /// This enumeration represents the different places of your natural divider.
@@ -18,9 +33,12 @@ public enum DividerPlaceSet: CaseIterable {
   case opa
   /// The second operand
   case opb
-  /// The result of `opa * opb`.
+  /// The result of `opa / opb`.
   case res
-
+	/// A flag that enables the refilling of `opa`.
+	case ena
+  /// Store the tokens to refill in `opa`.
+  case sto
   // Add your additional places here, if any.
 
 }
--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -19,20 +19,55 @@ public struct InhibitorNet<Place> where Place: Hashable {
     public let postconditions: [Place: Arc]
 
     /// A method that returns whether a transition is fireable from a given marking.
-    public func isFireable(from marking: [Place: Int]) -> Bool {
-      // Write your code here.
-      return false
-    }
+		public func isFireable(from marking: [Place: Int]) -> Bool {
+		 // Write your code here.
+		 for place in preconditions {
+			 switch preconditions[place.key]! { // on itère sur les arcs
+			 case .regular(let value): // si on a un arc "normal", on récupère la valeur de l'arc = précondition
+					 if marking[place.key]! < value { // si la précondition est plus grande que le marquage : non tirable
+						 return false
+					 }
+				 case .inhibitor: // si on a un arc inhibiteur
+					 if marking[place.key] != 0 { // si le marquage n'est pas 0, la transition n'est pas tirable
+						 return false
+					 }
+			 }
+		 }
+		 return true
+	 }
 
     /// A method that fires a transition from a given marking.
     ///
     /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
     /// otherwise it returns the new marking.
-    public func fire(from marking: [Place: Int]) -> [Place: Int]? {
+		public func fire(from marking: [Place: Int]) -> [Place: Int]? {
       // Write your code here.
-      return nil
-    }
+      if isFireable(from: marking){
+        var newMarking = marking // on récupère le marquage passé en argument
 
+        for place in preconditions { // on itère sur les arcs, dans les préconditions
+          switch preconditions[place.key]! {
+					case .regular(let value): // si arc normal (value = précondition)
+            newMarking[place.key]! -= value // on enlève au marquage la valeur demandée pour le tirage, donc le nombre pris pour tirer
+						break
+					case .inhibitor: // si arc inhibiteur
+              break // rien à faire, on tire juste
+          }
+        }
+
+        for place in postconditions{ //on itère sur les arcs, dans les postconditions
+          switch postconditions[place.key]! {
+					case .regular(let value): // si arc "normal"
+            newMarking[place.key]! += value // on ajoute au marquage le nombre gagné après le tirage
+						break
+					case .inhibitor: // si inhibiteur
+             break
+          }
+        }
+         return newMarking
+      }
+      return nil // si pas tirable, on retourne nil
+    }
   }
 
   /// Struct that represents an arc of a Petri net extended with inhibitor arcs.
