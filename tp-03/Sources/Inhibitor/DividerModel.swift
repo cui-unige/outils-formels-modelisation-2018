476cea1d938e495685c4c4609972d3cac2641e5f
Ella Kummer <ella.kummer@etu.unige.ch>
2018-11-16 09:24:22 +0100

    tp3comment

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -6,14 +6,17 @@ public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
     places: Set(DividerPlaceSet.allCases), // on garde toutes les places (les mêmes)
     transitions: [
       InhibitorNet.Transition(
+        // on enlève n de m
         name: "add", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.sto: 1]),
-				// Refills the tokens of `opa`.
+				// on re-repli opb à n (à l'aide de sto dans lequel on a stocké n durant add)
+        // quand on décrémentait n de m, en même temps on remplissait sto
       InhibitorNet.Transition(
         name: "rfl", pre: [.ena: 1, .sto: 1], post: [.ena: 1, .opb: 1]), // (ne change pas)
 				// Activates the refilling of `opa`.
       InhibitorNet.Transition(
+        // on met ena à 1, pour pouvoir reremplir opb au bon nombre (à l'aide de sto qui le contient)
         name: "ch1", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1]), // (ne change pas)
-				//
+        // on a enlevé n de m et "n de nouveau à n" : res = res + 1
       InhibitorNet.Transition(
         name: "ch2", pre: [.ena: 1, .sto: .inhibitor], post: [.res: 1]),
     ])
