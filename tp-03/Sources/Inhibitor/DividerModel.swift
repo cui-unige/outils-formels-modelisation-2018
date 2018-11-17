5a664671a8f727d0822f6ad0d6a9d46eb4ce316b
WatiBenj <benjamin.fischer@etu.unige.ch>
2018-11-17 10:26:34 +0100

    TP3 : FINAL VERSION
    BENJAMIN FISCHER

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,26 +1,77 @@
-/// This function creates the model of a natural divider.
+/*
+------------------------------------------------------------------------------
+# NAME : DividerModel.swift
+#
+# PURPOSE : Implementation of a natural divider model and its initial marking
+#
+# AUTHOR : Benjamin Fischer
+#
+# CREATED : 16.11.2018
+-----------------------------------------------------------------------------
+*/
+/*
+# ------------------------------------------------------------------------------
+# METHOD : createDividerModel() -> InhibitorNet<DividerPlaceSet>
+#
+# PURPOSE : Creates the model of a natural divider
+#
+# INPUT : -
+#
+# OUTPUT : InhibitorNet<DividerPlaceSet>
+# -----------------------------------------------------------------------------
+*/
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
-  // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+  return InhibitorNet(
+    places: Set(DividerPlaceSet.allCases), // keep the same places
+      transitions: [
+        // store opb in sto
+        // substraction : 'opa - opb'
+        InhibitorNet.Transition(
+          name: "sub", pre: [ .opa: 1, .opb: 1, .enb: .inhibitor], post: [.sto: 1]),
+        // refill opb (from sto) for next substraction
+        // empty sto for next substraction
+        // enb allows to fire sub
+        // once sto is empty, sub is not fireable anymore
+        InhibitorNet.Transition(
+          name: "rfl", pre: [ .sto: 1, .enb: 1], post: [ .enb: 1, .opb: 1]),
+        // ch1 set rfl when enb and opb are empty (inhibitor arcs)
+        // active enb
+        // ch1 is fireable when opb is empty ===> increment of 1 in res
+        // if opb is non-empty, there is no more incrementation and divider model will be blocked by opa
+        InhibitorNet.Transition(
+          name: "ch1", pre: [ .opb: .inhibitor, .enb: .inhibitor], post: [ .enb: 1, .res: 1]),
+        // ch2 restarts a new substraction (fills enb)
+        InhibitorNet.Transition(
+          name: "ch2", pre: [ .enb: 1, .sto : .inhibitor], post: [:]),
+    ])
 }
-
-/// This function returns the initial marking corresponding to the model of your divider, for two
-/// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
+/*
+# ------------------------------------------------------------------------------
+# METHOD : createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int]
+#
+# PURPOSE : Returns the initial marking corresponding to the model of the natural divider,
+#           for two operands 'opa' and 'opb' such that the model computes 'opa / opb'
+#
+# INPUT : opa: Int, opb: Int
+#
+# OUTPUT : [DividerPlaceSet: Int]
+# -----------------------------------------------------------------------------
+*/
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
-  // Write your code here.
-  return [:]
+  // divider's initial marking
+  return [.opa: opa, .opb: opb, .res: 0, .enb: 0, .sto:0 ]
 }
 
 /// This enumeration represents the different places of your natural divider.
 public enum DividerPlaceSet: CaseIterable {
-
   /// The first operand.
   case opa
   /// The second operand
   case opb
   /// The result of `opa * opb`.
   case res
-
-  // Add your additional places here, if any.
-
+  /// A flag that enables the refilling of `opb`.
+  case enb
+  /// Store the tokens to refill in `opb`.
+  case sto
 }
--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -1,3 +1,14 @@
+/*
+------------------------------------------------------------------------------
+# NAME : Inhibitor.swift
+#
+# PURPOSE : Extension of PetriNet structure containing inhibitor arcs
+#
+# AUTHOR : Benjamin Fischer
+#
+# CREATED : 16.11.2018
+-----------------------------------------------------------------------------
+*/
 public struct InhibitorNet<Place> where Place: Hashable {
 
   /// Struct that represents an transition of a Petri net extended with inhibitor arcs.
@@ -18,18 +29,96 @@ public struct InhibitorNet<Place> where Place: Hashable {
     public let preconditions: [Place: Arc]
     public let postconditions: [Place: Arc]
 
-    /// A method that returns whether a transition is fireable from a given marking.
+    /*
+    # ------------------------------------------------------------------------------
+    # METHOD : isFireable(from marking: [Place: Int]) -> Bool
+    #
+    # PURPOSE : Returns a boolean value depending on whether a transition is
+    #           fireable from a given marking
+    #
+    # INPUT : marking: [Place: Int]
+    #
+    # OUTPUT : TRUE if transition is fireable, FALSE if it is not
+    # -----------------------------------------------------------------------------
+    */
     public func isFireable(from marking: [Place: Int]) -> Bool {
-      // Write your code here.
-      return false
+
+    for place in preconditions { // loop on places of preconditions
+        switch preconditions[place.key]! {
+          
+          case .regular(let value): // case of regular arc
+            // value == regular arc precondition value
+            if marking[place.key]! < value {
+              // regular arc precondition value > marking of precondition place
+              return false
+            }
+
+          case .inhibitor: // case of inhibitor arc
+            if marking[place.key] != 0 {
+              // marking of precondition place is not empty
+              return false
+            }
+
+          }
+        }
+      // given marking is fireable
+      return true
     }
 
-    /// A method that fires a transition from a given marking.
-    ///
-    /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
-    /// otherwise it returns the new marking.
+    /*
+    # ------------------------------------------------------------------------------
+    # METHOD : fire(from marking: [Place: Int]) -> [Place: Int]?
+    #
+    # PURPOSE : Fires a transition from a given marking. If the transition is fireable
+    #           from the given marking, the method returns the new marking, otherwise
+    #           it returns a nil value.
+    #
+    # INPUT : from marking: [Place: Int]
+    #
+    # OUTPUT : New marking if transition is fireable, nil if it is not
+    # -----------------------------------------------------------------------------
+    */
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
-      // Write your code here.
+
+      if isFireable(from: marking) { // check if the given marking if fireable
+
+        var newMarking = marking // store the current marking
+
+        for place in preconditions { // loop on places of preconditions
+          switch preconditions[place.key]! {
+
+					case .regular(let value): // case of regular arc
+            // value == regular arc precondition value
+            // fire transition : substract regular arc precondition value from marking of precondition place
+            newMarking[place.key]! -= value
+						break // exit loop
+
+					case .inhibitor: // case of inhibitor arc
+            // fire
+            break // exit loop
+
+          }
+        }
+
+        for place in postconditions { // loop on places of postconditions
+          switch postconditions[place.key]! {
+
+  					case .regular(let value): // case of regular arc
+              // value == regular arc precondition value
+              // fire transition : add regular arc postcondition value to marking of postcondition place
+              newMarking[place.key]! += value
+  						break // exit loop
+
+  					case .inhibitor: // case of inhibitor arc
+              // fire
+              break // exit loop
+          }
+
+        }
+        // new marking after firing
+        return newMarking
+      }
+      // nil otherwise
       return nil
     }
 
