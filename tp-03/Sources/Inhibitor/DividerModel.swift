d2932f36069ab87295ae7b5a77f1bdc86ca3dfd3
Elham Amin Mansour <elhamaminmansour@macbook-pro-de-elham.home>
2018-11-17 12:57:56 +0100

    tp3

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -3,24 +3,7 @@ public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
   // Write your code here.
 
   /// The structure of the model. opa/opb   opa--
-  let net = InhibitorNet(
-    places: Set(DividerPlaceSet.allCases),
-    transitions: [
-      // Add tokens in `res` as long as there are some to consume in `opa` and `opb`.1 opa--
-      InhibitorNet.Transition(
-        name: "minus", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.sto: 1]),
-      // Refills the tokens of `opb`.3
-      InhibitorNet.Transition(
-        name: "rfl", pre: [.ena: 1, .sto: 1], post: [.ena: 1, .opb: 1]),
-      // Activates the refilling of `opb`.2 opb-
-      InhibitorNet.Transition(
-        name: "ch1", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1, .res:1]),
-      // Deactivates the refilling of `opa`.4
-      InhibitorNet.Transition(
-        name: "ch2", pre: [.ena: 1, .sto: .inhibitor], post: [:]),
-    ])
-  return net //InhibitorNet(places: [], transitions: [])
-}
+//here
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -1,79 +1,7 @@
 public struct InhibitorNet<Place> where Place: Hashable {
 
   /// Struct that represents an transition of a Petri net extended with inhibitor arcs.
-  public struct Transition: Hashable {
 
-    public init(name: String, pre: [Place: Arc], post: [Place: Arc]) {
-      for (_, arc) in post {
-        guard case .regular = arc
-          else { preconditionFailure() }
-      }
-
-      self.name = name
-      self.preconditions = pre
-      self.postconditions = post
-    }
-
-    public let name: String
-    public let preconditions: [Place: Arc]
-    public let postconditions: [Place: Arc]
-
-    /// A method that returns whether a transition is fireable from a given marking.
-    public func isFireable(from marking: [Place: Int]) -> Bool {
-      // Write your code here.
-
-        for (place, arc) in self.preconditions{
-
-        switch(arc){
-        case let .regular(Int: value):
-                          guard marking[place]! >= value else { return false }
-
-        case .inhibitor :
-                          guard marking[place]! == 0 else { return false }
-        }
-      }
-      return true
-  }
-
-    /// A method that fires a transition from a given marking.
-    ///
-    /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
-    /// otherwise it returns the new marking.
-    public func fire(from marking: [Place: Int]) -> [Place: Int]? {
-      // Write your code here.
-      var newMarking = marking
-      if !isFireable(from : marking){
-          return nil
-      }
-      else{
-
-            for (place, arc) in self.preconditions{
-
-                  switch(arc){
-                        case let .regular(Int: value):
-                                          newMarking[place] = newMarking[place]! - value
-                        case .inhibitor :
-                                          newMarking[place] = newMarking[place]!
-                }
-
-            }
-
-            for (place, arc) in self.postconditions{
-
-                  switch(arc){
-                        case let .regular(Int: value):
-                                          newMarking[place] = newMarking[place]! + value
-                        case .inhibitor :
-                                          newMarking[place] = newMarking[place]!
-                  }
-
-            }
-
-      }
-          return newMarking
-    }
-
-  }
 
   /// Struct that represents an arc of a Petri net extended with inhibitor arcs.
   public enum Arc: Hashable {
