8f157dfbaae3a8e3f7c10f10db34494d5ec8ba7d
Laurent Roro <le.robynet.cool@gmail.com>
2018-11-18 19:04:59 +0100

    TP6 finished

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,22 +1,31 @@
 /// This function creates the model of a natural divider.
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
 
-  // Write your code here.
+  /// on fait grosso modo la même chose que la multiplication,
+  /// sauf que le moment de remplissage de res ne se fait pas au même moment
+  /// cad : on incrémente res lorsqu'on a pu tirer "disable refill"
+
+  /// idée générale : on veut savoir combien de fois on peut soustraire opb à opa
+  /// add            : vide a et b jusqu'à ce que l'un des deux soit vide, et remplit sto
+  /// refill         : vide remt sto dans opb, si ena est à 1 (et le garde à 1) (si sto n'atteint pas la valeur originale de opb, c'est pas grave par ce que ça veut dire que de toute façon, opa est vide)
+  /// enable refill  : on peut vider sto dans opb
+  /// disable refill : on a fini une loop : on a réussi à mettre un opb dans opa, on incrémente res
+
   return InhibitorNet(
     places: Set(DividerPlaceSet.allCases),
     transitions: [
-      // InhibitorNet.Transition(
-      //   name: "iterate",
-      //   pre: [
-      //       .opa: 1,
-      //       .opb: 1,
-      //       .ena: .inhibitor],
-      //   post: [
-      //     .opb: 1,
-      //     .sto: 1,
-      //     .res: 1
-      //   ]
-      // ),
+
+      InhibitorNet.Transition(
+        name: "add", pre: [ .opa: 1, .opb: 1, .ena: .inhibitor ], post: [ .sto: 1 ]),
+
+      InhibitorNet.Transition(
+        name: "refill", pre: [ .ena: 1, .sto: 1 ], post: [ .ena: 1, .opb: 1 ]),
+
+      InhibitorNet.Transition(
+        name: "enable refill", pre: [ .opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1]),
+
+      InhibitorNet.Transition(
+        name: "disable refill", pre: [.ena: 1, .sto: .inhibitor], post: [.res: 1]),
     ])
 
 }
@@ -24,23 +33,24 @@ public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
-  return [
-    .opa: opa,
-    .opb: opb,
-    .res: 0
-  ]
+  return [.opa: opa, .opb: opb, .res: 0, .ena: 0, .sto: 0]
 }
 
 /// This enumeration represents the different places of your natural divider.
 public enum DividerPlaceSet: CaseIterable {
 
-  /// The first operand.
+  /// input  value : opa
   case opa
-  /// The second operand
+  /// input  value : opb
   case opb
-  /// The result of `opa * opb`.
+  /// output value : floor( opa / opb )
   case res
 
-  // Add your additional places here, if any.
+  // helper
+  /// A flag that enables the refilling of `opb`.
+  case ena
+
+  /// Store the tokens to refill in `opb`.
+  case sto
 
 }
