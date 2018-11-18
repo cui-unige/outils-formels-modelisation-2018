ca0036ce753516843f477c13d3662e8ba466666a
Jimmysburgger <Jean-Marc.Boutay@etu.unige.ch>
2018-11-18 12:40:54 +0100

    TP3_fin

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -4,16 +4,16 @@ public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
   return InhibitorNet(
     places: Set(DividerPlaceSet.allCases),
     transitions: [
-      // Add tokens in `res` as long as there are some to consume in `opa` and `opb`.
+      // add ajoute des jetons dans res à chaque fois qu'on peut enlever opb de opa
       InhibitorNet.Transition(
         name: "add", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.sto: 1]),
-      // Refills the tokens of `opa`.
+      // rfl reremplis opb
       InhibitorNet.Transition(
         name: "rfl", pre: [.ena: 1, .sto: 1], post: [.ena: 1, .opb: 1]),
-      // Activates the refilling of `opa`.
+      // ch1 active le reremplissement de opb
       InhibitorNet.Transition(
         name: "ch1", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1]),
-      // Deactivates the refilling of `opa`.
+      // ch2 désactive le reremplissement de opb.
       InhibitorNet.Transition(
         name: "ch2", pre: [.ena: 1, .sto: .inhibitor], post: [.res: 1]),
     ])
@@ -21,19 +21,18 @@ public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
-  // Write your code here.
   return [.opa: opa, .opb: opb, .res: 0, .ena: 0, .sto: 0]
 }
 /// This enumeration represents the different places of your natural divider.
 public enum DividerPlaceSet: CaseIterable {
-  /// The first operand.
+  /// opa est la 1ère opérande.
   case opa
-  /// The second operand.
+  /// opb est la 2ème opérande.
   case opb
-  /// The result of opa / opb`.
+  /// res est le résultat de opa / opb`.
   case res
-  /// A flag that enables the refilling of `opa`.
+  /// ena est un flag qui permet le reremplissement de `opb` (quand il vaut 1).
   case ena
-  /// Store the tokens to refill in `opa`.
+  /// sto contient les jetons pour remplir `opb`.
   case sto
 }
