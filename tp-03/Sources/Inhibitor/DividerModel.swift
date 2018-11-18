28a67f4a605d100247ca70d16bbff45bacf675bc
Rose <rose@dell.home>
2018-11-18 13:24:10 +0100

    Ajout DividerModel et DividerInitialMarkin

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,14 +1,25 @@
 /// This function creates the model of a natural divider.
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
-  // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+  let net = InhibitorNet(
+    places: Set(DividerPlaceSet.allCases),
+    transitions: [
+      InhibitorNet.Transition(
+        name: "moins", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.stock: 1]),
+      InhibitorNet.Transition(
+        name: "recharge", pre: [.stock: 1, .ena: 1], post: [.ena: 1, .opb: 1]),
+      InhibitorNet.Transition(
+        name: "active", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.res: 1, .ena: 1]),
+      InhibitorNet.Transition(
+        name: "desactive", pre: [.stock: .inhibitor, .ena: 1], post: [:]),
+    ])
+  return net
 }
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
   // Write your code here.
-  return [:]
+  return [.opa: opa, .opb: opb, .res: 0, .ena: 0, .stock: 0]
 }
 
 /// This enumeration represents the different places of your natural divider.
@@ -20,7 +31,9 @@ public enum DividerPlaceSet: CaseIterable {
   case opb
   /// The result of `opa * opb`.
   case res
-
-  // Add your additional places here, if any.
+  /// enable
+  case ena
+  //stock
+  case stock
 
 }
