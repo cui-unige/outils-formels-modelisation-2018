e805605cd12ee715d387369ca7812f17d9f96d1a
mark <mark.tropin@etu.unige.ch>
2018-11-19 21:08:00 +0100

    Model done, final commit

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,19 +1,37 @@
-/// This function creates the model of a natural divider.
+// This model implements integer division.
+
+  // It works as follows:
+  // 1. copy n tokens from opb to opb_copy_buffer
+  // 2. if opb is empty, add one token to res
+  // 3. refill opb from buffer
+  // 4. check if m = n = 0. if so, make all transitions unfireable (this is checked with opa and opb_copy_buffer being 0)
+
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
-  // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+  let myDivider = InhibitorNet(
+    places: Set(DividerPlaceSet.allCases),
+    transitions: [
+      InhibitorNet.Transition(
+          name: "copy_b", pre: [.opa: 1, .opb: 1, .refill: .inhibitor], post: [.opb_copy_buffer: 1]),
+      InhibitorNet.Transition(
+          name: "enable_refill", pre: [.opb: .inhibitor, .refill: .inhibitor], post: [.refill: 1, .res: 1]),
+      InhibitorNet.Transition(
+          name: "refill", pre: [.refill: 1, .opb_copy_buffer: 1], post: [.refill: 1, .opb: 1]),
+      InhibitorNet.Transition(
+          name: "disable_refill", pre: [.refill: 1, .opb_copy_buffer: .inhibitor], post: [:]),
+    ])
+  return myDivider
 }
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
-  // Write your code here.
-  return [:]
+  let initMarking: [DividerPlaceSet: Int] = [.opa: opa, .opb: opb, .res: 0, .refill: 0, .opb_copy_buffer: 0]
+
+  return initMarking
 }
 
 /// This enumeration represents the different places of your natural divider.
 public enum DividerPlaceSet: CaseIterable {
-
   /// The first operand.
   case opa
   /// The second operand
@@ -21,6 +39,8 @@ public enum DividerPlaceSet: CaseIterable {
   /// The result of `opa * opb`.
   case res
 
-  // Add your additional places here, if any.
-
+  // Refilling flag
+  case refill
+  // A buffer that functions as a copy of opb
+  case opb_copy_buffer
 }
