4e0c499a60a725552f35cdfec130d129c4557b29
ikam6 <Ikam6@macbook-pro-de-marko.home>
2018-11-19 23:42:24 +0100

    rendu TP3

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,14 +1,36 @@
 /// This function creates the model of a natural divider.
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
-  // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+  // Créer un PetriNet pour faire la division avec opa/opb :
+
+  return InhibitorNet(
+    places: Set( DividerPlaceSet.allCases ),
+    transitions: [
+      InhibitorNet.Transition(
+        // soustraire opa-opb et stocker dans opa.
+        // stocker nombre de jeton de opb jusqu'à avoir opa < opb
+        name: "sub", pre: [.opa: 1, .opb: 1, .enb: .inhibitor], post: [.sto: 1]
+      ),
+      InhibitorNet.Transition(
+        // quand opb vide, incrémenter res et enb
+        // quand opb > opa alors plus tirable donc resultat de la divison dans res
+        name: "1", pre: [.opb: .inhibitor, .enb: .inhibitor], post: [.enb: 1, .res: 1]
+      ),
+      InhibitorNet.Transition(
+        // vider sto et remplir a nouveau opb pour prochaine soustraction
+        name: "2", pre: [.enb: 1, .sto: 1], post: [.enb: 1, .opb: 1]
+      ),
+      InhibitorNet.Transition(
+        // enfin on vide enb
+        name: "3", pre: [.enb: 1, .sto: .inhibitor], post: [:]
+      ),
+    ])
 }
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
-  // Write your code here.
-  return [:]
+  // marquage initial du Divider Model
+  return [.opa: opa, .opb: opb, .res: 0, .enb: 0, .sto: 0]
 }
 
 /// This enumeration represents the different places of your natural divider.
@@ -22,5 +44,10 @@ public enum DividerPlaceSet: CaseIterable {
   case res
 
   // Add your additional places here, if any.
+  // socker le nombre de jeton jusqu'a opa < opb
+  case sto
+
+  // pour la répétition du processus dans division
+  case enb
 
 }
--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -20,8 +20,27 @@ public struct InhibitorNet<Place> where Place: Hashable {
 
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
-      // Write your code here.
-      return false
+      // Similaire au tp-01:
+      // Pour toutes les places de préconditions ne sera pas tirable si:
+      // 1. l'arc est inhibiteur et non-null
+      // 2. l'arc est normal et que le poids est supérieur au marquage
+      // sinon la transition est tirable
+      for ( place, arc ) in self.preconditions {
+        switch arc {
+        case .inhibitor:
+          if marking[place] != 0 {
+            return false
+          }
+
+        case .regular( Int: let x ):
+          if marking[place]! < x {
+            return false
+          }
+        // default:
+        //   print( "Problem" )
+        }
+      }
+      return true
     }
 
     /// A method that fires a transition from a given marking.
@@ -29,8 +48,41 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
-      // Write your code here.
-      return nil
+      // Similaire à tp-01 :
+      // Si transition non tirable on retourne nil :
+      if( isFireable(from: marking) == false ) {
+        return nil
+      }
+
+      // Si transition est tirable :
+      // On créer un marquage temporaire puis on regarde les pre et post conditions
+      // sur toutes les places en vérifiant les arcs inhibiteur et normaux :
+      var markingTmp: [ Place: Int ] = marking
+
+      // pré conditions :
+      // si arc inhibiteur on continue
+      // si arc normal on enleve un marquage
+      for ( place, arc ) in self.preconditions {
+          switch arc {
+          case .inhibitor:
+            continue
+          case .regular( Int: let x ):
+            markingTmp[place]! -= x
+          }
+      }
+
+      // post conditions :
+      // si arc inhibiteur on continue
+      // si arc normal on ajoute un marquage
+      for ( place, arc ) in self.postconditions {
+          switch arc {
+          case .inhibitor:
+            continue
+          case .regular( Int: let x ):
+            markingTmp[place]! += x
+          }
+      }
+      return markingTmp //on retourne le marquage temporaire
     }
 
   }
