4af6c7eabccd3b8456f5d3446b1aee0c6d16a73d
sasha <sasha.poirier@gmail.com>
2018-11-19 10:45:30 +0100

    TP3

--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -21,7 +21,24 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
       // Write your code here.
-      return false
+      for (place,arc) in self.preconditions //for all the places and arc in the preconditions
+      {
+        switch arc  //for each arc of either type
+        {
+          case .inhibitor:  //if the arc is of type inhibitor
+            if (marking[place] != 0)  //if the marking has something other than 0 tokens
+            {
+              return false  //return false
+            }
+          case .regular(let tokens):  //else if regular arc, dewraps the number of tokens on the arc
+            //the ! here is not a NOT, but an optional, as Int must be unwrapped as it is an Int? returned
+            if (marking[place]! < tokens) //if the number of tokens in the place for the given marking is less than the condition
+            {
+              return false  //return false
+            }
+        }
+      }
+      return true //otherwise the transition can be fired and true is returned
     }
 
     /// A method that fires a transition from a given marking.
@@ -30,7 +47,37 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
       // Write your code here.
-      return nil
+      //return nil
+      if self.isFireable(from: marking)
+      {
+        var newMarking: [Place: Int] = marking
+        for (place, arc) in self.preconditions
+        {
+          switch arc
+          {
+            case .inhibitor:  //as the isFireable already checks if the transition is fireable, there is nothing to do here
+                continue
+            case .regular(let tokens): //else if regular arc, dewraps the number of tokens on the arc, and removes them from the current place (for loop)
+              newMarking[place]! -= tokens
+          }
+        }
+
+        for (place, arc) in self.postconditions
+        {
+          switch arc
+          {
+            case .inhibitor:  //nothing to do here, basically just checks that the place is empty (from isFireable), so no token moving
+                continue
+            case .regular(let tokens): //else if regular arc, dewraps the number of tokens on the arc, and adds them to the the current place (for loop)
+              newMarking[place]! += tokens
+          }
+        }
+        return newMarking
+      }
+      else
+      {
+        return nil
+      }
     }
 
   }
