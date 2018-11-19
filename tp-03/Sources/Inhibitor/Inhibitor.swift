8ec364159936619454022dea8dccf7a9d1d6cee2
iliasmj <iliasmj@gmail.com>
2018-11-19 16:52:07 +0100

    TP03

--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -21,7 +21,25 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
       // Write your code here.
-      return false
+
+      //pour chaque place contenue dans les préconditions de la place ciblée
+      for place in preconditions{
+        switch preconditions[place.key]! {
+          //si la place est liée par un ihnibiteur
+          case .inhibitor:
+            //la transition est tirable que si le marquage est nul
+            if marking[place.key]! != 0 {
+              return false
+            }
+          //si la place n'est pas liée par un ihnibiteur
+          case .regular(let val):
+              //la place est tirable si le marquage est supérieur à la valeur englobée dans l'arc
+              if marking[place.key]! < val {
+                return false
+              }
+        }
+      }
+      return true
     }
 
     /// A method that fires a transition from a given marking.
@@ -30,6 +48,41 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
       // Write your code here.
+
+      //si la transition est tirable depuis le marquage choisi:
+      //le nouveau marquage se contruit de la sorte : marquage choisi + entrées + sorties
+      if isFireable(from: marking){
+        //on crée le nouveau marquage en l'initialisant avec les valeurs du marquage choisi
+        var marquageResultant = marking
+
+        //pour chaque place ayant des préconditions,
+        //on construit le nouveau marquage en soustrayant la valeur contenue
+        // dans l'arc de précondition à la valeur initalement contenue dans la place
+        for place in preconditions {
+          switch preconditions[place.key]! {
+            case .regular(let val):
+              marquageResultant[place.key]! -= val
+            case .inhibitor:
+              break
+          }
+        }
+
+        //pour chaque place ayant des postconditions,
+        //on construit le nouveau marquage en additionnant la valeur contenue
+        // dans l'arc de postcondition à la valeur initalement contenue dans la place
+        for place in postconditions {
+          switch postconditions[place.key]! {
+            case .regular(let val):
+              marquageResultant[place.key]! += val
+            case .inhibitor:
+              break
+          }
+        }
+
+        //on retourne enfin le marquage marquageResultant
+        return marquageResultant;
+      }
+
       return nil
     }
 
