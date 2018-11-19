fc74d73afa653ad6fbc8d863588aebee9b9f4109
SubiSun <subanki.suntharanaathan@etu.unige.ch>
2018-11-19 20:21:57 +0100

    TP3 rendu

--- tp-03/Sources/Inhibitor/DividerModel.swift
+++ tp-03/Sources/Inhibitor/DividerModel.swift
@@ -1,14 +1,25 @@
 /// This function creates the model of a natural divider.
 public func createDividerModel() -> InhibitorNet<DividerPlaceSet> {
   // Write your code here.
-  return InhibitorNet(places: [], transitions: [])
+  return InhibitorNet(
+  places: Set (DividerPlaceSet.allCases),
+  transitions: [
+    InhibitorNet.Transition( //enlève n de m.
+      name: "add", pre: [.opa: 1, .opb: 1, .ena: .inhibitor], post: [.sto: 1]), //sto crée sauvegarde
+    InhibitorNet.Transition(
+      name: "rfl", pre: [.ena: 1, .sto: 1], post: [.ena: 1, .opb: 1]),
+    InhibitorNet.Transition(
+      name: "ch1", pre: [.opb: .inhibitor, .ena: .inhibitor], post: [.ena: 1]),
+    InhibitorNet.Transition(
+      name: "ch2", pre: [.ena: 1, .sto: .inhibitor], post: [.res: 1])
+  ])
 }
 
 /// This function returns the initial marking corresponding to the model of your divider, for two
 /// operands `lhs` and `rhs` such that the model will compute `lhs / rhs`.
 public func createDividerInitialMarking(opa: Int, opb: Int) -> [DividerPlaceSet: Int] {
   // Write your code here.
-  return [:]
+  return [.opa: opa, .opb: opb, .res: 0, .ena: 0, .sto: 0]
 }
 
 /// This enumeration represents the different places of your natural divider.
@@ -18,9 +29,12 @@ public enum DividerPlaceSet: CaseIterable {
   case opa
   /// The second operand
   case opb
-  /// The result of `opa * opb`.
+  /// The result of `opa / opb`.
   case res
 
   // Add your additional places here, if any.
-
+  //place permettant de remplir opb
+  case ena
+  //place stockant les jetons à placer dans opb
+  case sto
 }
--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -21,7 +21,19 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
       // Write your code here.
-      return false
+      for place in preconditions {
+        switch preconditions[place.key]! {
+        case .inhibitor: //si arc inhibiteur
+            if (marking[place.key]! != 0) { //si marquage n'est pas égal à 0, transition non tirable.
+              return false
+            }
+          case .regular(let arcValue):
+            if (marking[place.key]! < arcValue) {//si marquage inférieur à valeur de l'arc (précondition), alors non tirable.
+              return false
+            }
+          }
+        }
+      return true
     }
 
     /// A method that fires a transition from a given marking.
@@ -30,6 +42,29 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
       // Write your code here.
+      if (self.isFireable(from: marking)) {
+        var newMarking = marking //initialiser nouveau marquage avec valeurs du marquage en question.
+        for place in preconditions {
+          switch preconditions[place.key]! {
+            case .regular(let arcValue):
+              newMarking[place.key]! = newMarking[place.key]! - arcValue
+              break
+            case .inhibitor:
+              break
+          }
+        }
+
+        for place in postconditions {
+          switch postconditions[place.key]! {
+            case .regular(let arcValue):
+              newMarking[place.key]! = newMarking[place.key]! + arcValue
+              break
+            case .inhibitor:
+              break
+          }
+        }
+        return newMarking
+      }
       return nil
     }
 
