ca2ab6ee6dc26ea7701686d369529fc76187733e
mark <mark.tropin@etu.unige.ch>
2018-11-19 19:18:42 +0100

    First exercice, first commit

--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -20,17 +20,72 @@ public struct InhibitorNet<Place> where Place: Hashable {
 
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
-      // Write your code here.
-      return false
-    }
+      // If the transition doesn't have an inhibitor arc, we treat it as a classical Petri net transition:
+      for (pl, precondArc) in preconditions
+      {
+        switch precondArc
+        // There are two cases here: either it's an inhibitor arc,
+        // or it's a normal arc. In the former case, the transition is 
+        // fireable given that the marking of the preceding place is 0.
+        // In the latter case, we treat it as a classical Petri net transition.
+        {
+          case .regular(let precond): // precond is the number of tokens removed by the firing
+            if (marking[pl]! < precond) // ! because optional
+            {
+              return false
+            }
+
+          case .inhibitor:
+            if (marking[pl] != 0)
+            {
+              return false
+            }
+          }
+        }
+        return true
+      }
+        
+
+
 
     /// A method that fires a transition from a given marking.
     ///
     /// If the transition isn't fireable from the given marking, the method returns a `nil` value.
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
-      // Write your code here.
-      return nil
+      // check if transition is fireable
+      if (!isFireable(from: marking))
+      {
+        return nil
+      } else {
+        var myMarking : [Place:Int] = marking
+        // We can have inhibitor arcs as preconditions or postconditions.
+        // That's why we treat preconditions and postconditions as two separate cases:
+        
+        // preconditions
+        for (pl, precondArc) in self.preconditions {
+          switch precondArc {
+            case .regular(let precond):
+              myMarking[pl]! = myMarking[pl]! - precond
+            case .inhibitor:
+              continue
+           }
+         }
+
+
+        // postconditions
+        for (pl, postcondArc) in self.postconditions {
+          switch postcondArc {
+            case .regular(let postcond):
+              myMarking[pl]! = myMarking[pl]! + postcond
+            case .inhibitor:
+              continue
+           }
+         }
+        
+
+        return myMarking
+      }
     }
 
   }
