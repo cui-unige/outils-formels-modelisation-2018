4e7992767551ac0c23103c6f8caddec366acf69d
DorianWeber <dorian.weber@etu.unige.ch>
2018-11-19 22:00:51 +0100

    Tp 3

--- tp-03/Sources/Inhibitor/Inhibitor.swift
+++ tp-03/Sources/Inhibitor/Inhibitor.swift
@@ -21,7 +21,20 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// A method that returns whether a transition is fireable from a given marking.
     public func isFireable(from marking: [Place: Int]) -> Bool {
       // Write your code here.
-      return false
+      for place in preconditions {    // on parcourt les places des preconditions
+        switch preconditions[place.key]! {
+        case .regular(let value): // si on a un arc regulier
+            // value = "valeur de l'arc regulier de la precondition"
+            if marking[place.key]! < value { // marquage precondition
+              return false
+            }
+          case .inhibitor: // si on a un arc inhibitor
+            if marking[place.key] != 0 { // marquage precondition
+              return false
+            }
+          }
+        }
+      return true // marquage est tirable
     }
 
     /// A method that fires a transition from a given marking.
@@ -30,8 +43,36 @@ public struct InhibitorNet<Place> where Place: Hashable {
     /// otherwise it returns the new marking.
     public func fire(from marking: [Place: Int]) -> [Place: Int]? {
       // Write your code here.
-      return nil
-    }
+      if isFireable(from: marking) { // on verifie si le marquage actuel est tirable
+        var newMarking = marking // arrete le marquage
+        for place in preconditions { // boucle sur preconditions
+          switch preconditions[place.key]! {
+            case .regular(let value): // si on a un arc regulier
+              // value = "valeur de l'arc regulier de la precondition"
+              newMarking[place.key]! -= value
+              break // on quitte la boucle
+            case .inhibitor: // si on a un arc inhibitor
+              // on tire
+              break // on quitte la boucle
+          }
+        }
+        for place in postconditions { // loop on places of postconditions
+          switch postconditions[place.key]! {
+            case .regular(let value): // si on a un arc regulier
+              // value = "valeur de l'arc regulier de la precondition"
+              newMarking[place.key]! += value
+              break // on quitte la boucle
+            case .inhibitor: // si on a un arc inhibitor
+              // on tire
+              break // on quitte la boucle
+          }
+        }
+        // nouveau marquage après avoir tiré
+        return newMarking
+      }
+        // nil sinon
+        return nil
+      }
 
   }
 
