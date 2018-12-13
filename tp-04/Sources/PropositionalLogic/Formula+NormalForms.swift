1d8cc8aab76d3447e4de869254e2b7e959c2b9a7
kkf17 <favre.kathleen@orange.fr>
2018-12-13 16:59:22 +0100

    rendu

--- tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
+++ tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
@@ -1,33 +1,147 @@
 extension Formula {
 
   /// The negation normal form of the formula.
+
   public var nnf: Formula {
     // Write your code here.
-    return self
+
+    switch self {
+       case .constant(let c):
+        return c ? true : false
+
+        case .proposition(_):
+          return self
+
+        case .negation(let a):
+          switch a {
+            case .constant(let c):
+              return c ? false : true
+            case .proposition(_):
+              return self
+            case .negation(let b):
+              return b.nnf
+            case .disjunction(let b, let c):
+              return (!b).nnf && (!c).nnf
+            case .conjunction(let b,let c):
+              return (!b).nnf || (!c).nnf
+            case .implication(_):
+              return (!a.nnf).nnf
+          }
+
+        case .disjunction(let b,let c):
+          return b.nnf || c.nnf
+
+        case .conjunction(let b,let c):
+          return b.nnf && c.nnf
+
+        case .implication(let b,let c):
+          return (!b).nnf || c.nnf
+    }
+
   }
 
   /// The disjunctive normal form (DNF) of the formula.
+
   public var dnf: Formula {
     // Write your code here.
-    return self
+
+    var form=self.nnf
+    //form=(!form).nnf
+    //Swift.print("DANS DNF: \(form)")
+
+    switch form {
+      case .constant(let c):
+       return c ? true : false
+
+       case .proposition(_):
+         return self
+
+       case .negation(let a):
+            return !a.dnf
+
+       case .disjunction(let b,let c):
+         return b.dnf || c.dnf
+
+       case .conjunction(let b,let c):
+         switch b {
+             case .disjunction(let d,let e):
+                 return (d.dnf && c.dnf) || (e.dnf && c.dnf)
+             default:
+               switch c {
+                 case .disjunction(let d,let e):
+                   return (b.dnf && d.dnf) || (b.dnf && e.dnf)
+                 default:
+                   return b.dnf && c.dnf
+               }
+         }
+
+        case .implication(let b,let c):
+          return ((!(self.nnf)).nnf).dnf
+    }
   }
 
   /// The conjunctive normal form (CNF) of the formula.
+
   public var cnf: Formula {
     // Write your code here.
-    return self
+
+    var form=self.nnf
+    //form=(!form).nnf
+    //Swift.print("DANS DNF: \(form)")
+
+    switch form {
+      case .constant(let c):
+       return c ? true : false
+
+       case .proposition(_):
+         return self
+
+       case .negation(let a):
+            return !a.dnf
+
+       case .conjunction(let b,let c):
+         return b.dnf && c.dnf
+
+       case .disjunction(let b,let c):
+         switch b {
+             case .conjunction(let d,let e):
+                 return (d.dnf || c.dnf) && (e.dnf || c.dnf)
+             default:
+               switch c {
+                 case .conjunction(let d,let e):
+                   return (b.dnf || d.dnf) && (b.dnf || e.dnf)
+                 default:
+                   return b.dnf || c.dnf
+               }
+         }
+
+        case .implication(let b,let c):
+          return ((!(self.nnf)).nnf).dnf
+    }
   }
 
   /// The minterms of a formula in disjunctive normal form.
+
   public var minterms: Set<Set<Formula>> {
     // Write your code here.
-    return []
+
+    var a:Set<Set<Formula>> = []
+    for p in disjunctionOperands{
+        a.insert(p.conjunctionOperands)
+    }
+    return a
   }
 
   /// The maxterms of a formula in conjunctive normal form.
+
   public var maxterms: Set<Set<Formula>> {
     // Write your code here.
-    return []
+
+    var a:Set<Set<Formula>> = []
+    for p in self.conjunctionOperands{
+        a.insert(p.disjunctionOperands)
+    }
+    return a
   }
 
   /// Unfold a tree of binary disjunctions into a set of operands.
--- tp-04/Sources/Sandbox/main.swift
+++ tp-04/Sources/Sandbox/main.swift
@@ -7,3 +7,39 @@ let s: Formula = "s"
 
 let formula = (p || q || r) && (!p || q)
 print("formula : \(formula)")
+
+
+Swift.print("\n Test pour les differentes fonctions: \n ")
+let a: Formula = "a"
+let b: Formula = "b"
+let c: Formula = "c"
+
+Swift.print("Test pour la fonction nnf de negation:  ")
+print((!(a || b)).nnf)
+
+Swift.print("\n Test pour la fonction dnf:  ")
+print((!(a => b => c)).dnf)
+
+Swift.print("\n Test pour la fonction cnf: ")
+print((!(a => b => c)).cnf)
+
+Swift.print("\n Test pour la fonction minterms: ")
+print((a || (a && b) || (a && c)).minterms)
+
+Swift.print("\n Test pour la fonction maxterms: ")
+print((a && (a || b) && (a || c)).maxterms)
+
+
+Swift.print("\n TEST POUR NEGATION NORMAL FORM\n")
+
+Swift.print("\(Formula.constant(true).nnf)" )
+Swift.print("\(a.nnf)" )
+Swift.print("\((!Formula.constant(true)).nnf)" )
+Swift.print("\((!a).nnf)" )
+Swift.print("\((!(!a)).nnf)" )
+Swift.print("\((!(a || b)).nnf)" )
+Swift.print("\((!(a && b)).nnf)" )
+Swift.print("\((!(a => b)).nnf)" )
+Swift.print("\((a || b).nnf)" )
+Swift.print("\((a && b).nnf)" )
+Swift.print("\((a => b).nnf)" )
