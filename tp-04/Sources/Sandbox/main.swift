8d658b8dc6bd62f448ad446864f4a7b70f30d92b
Marc Heimendinger <marc@Marcs-MacBook-Pro-2.local>
2018-12-13 12:03:02 +0100

    TP4

--- tp-04/Sources/Sandbox/main.swift
+++ tp-04/Sources/Sandbox/main.swift
@@ -1,9 +1,19 @@
 import PropositionalLogic
 
-let p: Formula = "p"
-let q: Formula = "q"
-let r: Formula = "r"
-let s: Formula = "s"
+// let p: Formula = "p"
+// let q: Formula = "q"
+// let r: Formula = "r"
+// let s: Formula = "s"
 
-let formula = (p || q || r) && (!p || q)
-print("formula : \(formula)")
+// let formula = (p || q || r) && (!p || q)
+// print("formula : \(formula)")
+
+let a: Formula = "a"
+let b: Formula = "b"
+let c: Formula = "c"
+
+let formula = ((a && a) || (a && b) || (a && c) || (b && c) || (a && b && c)).dnf
+print("Formula : \(formula)")
+// Output :   (((((a ∧ a) ∨ (a ∧ b)) ∨ (a ∧ c)) ∨ (b ∧ c)) ∨ ((a ∧ b) ∧ c))
+// Output 2 : ((((a ∨ (a ∧ b)) ∨ (a ∧ c)) ∨ (b ∧ c)) ∨ ((a ∧ b) ∧ c))
+// Output 3 : ((((a ∨ (a ∧ b)) ∨ (a ∧ c)) ∨ (b ∧ c)) ∨ ((a ∧ b) ∧ c))
