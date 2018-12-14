a87a74a400f16de8f7ca9fc02ebbed6f9aabed67
Christopher Staub <christopherstaub@ChristophersMBP.fritz.box>
2018-12-14 22:32:59 +0100

    commit changes

--- tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
+++ tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
@@ -2,33 +2,177 @@ extension Formula {
 
   /// The negation normal form of the formula.
   public var nnf: Formula {
-    // Write your code here.
-    return self
-  }
+    switch self {
+    case .implication(let a_implique,let b):
+      return (!a_implique).nnf || (b).nnf
+    case .conjunction(let a_et , let b):
+      return (a_et).nnf || (b).nnf
+    case .disjunction(let a_ou , let b):
+      return (a_ou).nnf && (b).nnf
+    case .constant(let const):
+      return const ? true : false
+    case .proposition(_):
+      return self
+    case .negation(let formula):
+
+          switch formula{
+            case .constant(let cons):
+              return cons ? false : true
+            case .proposition(_):
+              return self
+            case .negation(let neg):
+              return neg.nnf
+            case .conjunction(let a_et , let b):
+              return ((!a_et).nnf) || ((!b).nnf)
+            case .disjunction(let a_ou , let b):
+              return((!a_ou).nnf) && ((!b).nnf)
+            case .implication(let a_implique,let b):
+              return (a_implique).nnf && (!b).nnf
+        }
+      }
+    }
 
   /// The disjunctive normal form (DNF) of the formula.
   public var dnf: Formula {
-    // Write your code here.
-    return self
-  }
+     switch self.nnf { //on fait un nnf
 
+//cas conjonction
+    case .conjunction:
+      var operands = self.nnf.conjunctionOperands
+    //but: separation en disjonction
+      if let firstDisjunction = operands.first(where: { //separation des operandes
+              if case .disjunction = $0 { //recherche de disjonction
+                return true //true si trouvé
+              }
+            return false }) //false sinon
+      {
+        if case let .disjunction(a,b) = firstDisjunction {
+          operands.remove(firstDisjunction) //suppression de l'opérande car redondance
+          var res : Formula?
+          for op in operands { //itération sur opérandes des opérandes
+            if res != nil {
+              res = res! && op //concaténation
+             }
+            else {
+              res = op
+            }
+          }
+        res = (a.dnf && res!.dnf) || (b.dnf && res!.dnf)
+          if res != nil { return res!.dnf}
+      }
+    }
+    return self.nnf
+//cas disjonction
+  case .disjunction: //
+    var operands = self.disjunctionOperands
+    for op in operands {
+      for op2 in operands {
+        if op.conjunctionOperands.isSubset(of: op2.conjunctionOperands) && op2 != op {
+          operands.remove(op2)
+        }
+      }
+    }
+    var res : Formula?
+    for op in operands {
+      if res != nil {
+        res = res! || op
+      }
+      else {
+        res = op
+      }
+    }
+    return res!
+
+  default :
+    return self.nnf
+    }
+  }
   /// The conjunctive normal form (CNF) of the formula.
   public var cnf: Formula {
-    // Write your code here.
-    return self
+    switch self.nnf {
+ //cas disjonction
+
+    case .disjunction:
+      var operandes = self.nnf.disjunctionOperands
+      if let firstConjunction = operandes.first(where: { if case  .conjunction = $0 {
+        return true
+      }
+      return false }) {
+          if case let .conjunction(a, b) = firstConjunction {
+            operandes.remove(firstConjunction)
+            var resultat : Formula?
+            for operandeA in operandes {
+              if resultat != nil  {
+                resultat = resultat! || operandeA
+              }
+              else {
+                resultat = operandeA
+              }
+            }
+          resultat = (a.cnf || resultat!.cnf) && (b.cnf || resultat!.cnf)
+          if resultat != nil { return resultat!.cnf}
+          }
+      }
+      return self.nnf
+  //cas conjonction
+    case .conjunction:
+      var operandes = self.nnf.conjunctionOperands
+      for operandeA in operandes {
+        for operandeB in operandes {
+          if operandeA.disjunctionOperands.isSubset(of:operandeB.disjunctionOperands) && operandeB != operandeA {
+            operandes.remove(operandeB)
+          }
+        }
+      }
+      var resultat : Formula?
+      for operandeA in operandes {
+        if resultat != nil  {
+          resultat = resultat! && operandeA
+        }
+        else {
+          resultat = operandeA
+        }
+      }
+      return resultat!
+    default :
+      return self.nnf
+    }
   }
+
+
 
   /// The minterms of a formula in disjunctive normal form.
   public var minterms: Set<Set<Formula>> {
-    // Write your code here.
-    return []
-  }
+    switch self {
 
+    case .disjunction:
+      let operands = self.disjunctionOperands
+      var resultat : Set<Set<Formula>> = []
+      for operande in operands {
+        resultat.insert(operande.conjunctionOperands)
+      }
+      return resultat
+    default:
+      return []
+
+    }
+  }
   /// The maxterms of a formula in conjunctive normal form.
   public var maxterms: Set<Set<Formula>> {
-    // Write your code here.
-    return []
-  }
+    switch self {
+
+      case .conjunction:
+        let operands = self.conjunctionOperands
+        var resultat : Set<Set<Formula>> = []
+        for operande in operands {
+            resultat.insert(operande.disjunctionOperands)
+          }
+          return resultat
+        default:
+          return []
+        }
+    }
+
 
   /// Unfold a tree of binary disjunctions into a set of operands.
   ///
--- tp-04/Sources/Sandbox/main.swift
+++ tp-04/Sources/Sandbox/main.swift
@@ -6,4 +6,12 @@ let r: Formula = "r"
 let s: Formula = "s"
 
 let formula = (p || q || r) && (!p || q)
+//let tauto = !(p&&(!p))
+//let formula1 = !(p&&(!q)&&r)
+//let formula2 = !(!(q))
+//let formula3 = formula1.isSemanticallyEquivalent(to:formula2)
+//let formula4 = !(p=>q=>r)
+//let formula5 = (p || q || r) && (!p || q) && !(p=>q=>r) || !(s=>p) || (s||r) && (r)
+//let value = formula4.eval(with: ["p": true, "q": false,"r":true] )
+//let formula3 = formula1.isSemanticallyEquivalent(to:formula2)
 print("formula : \(formula)")
