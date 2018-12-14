900db66d9f163b04d1289fa8a130084ee6812aae
Christopher Staub <christopherstaub@ChristophersMBP.fritz.box>
2018-12-14 23:06:08 +0100

    commit changes

--- tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
+++ tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
@@ -3,175 +3,103 @@ extension Formula {
   /// The negation normal form of the formula.
   public var nnf: Formula {
     switch self {
-    case .implication(let a_implique,let b):
-      return (!a_implique).nnf || (b).nnf
-    case .conjunction(let a_et , let b):
-      return (a_et).nnf || (b).nnf
-    case .disjunction(let a_ou , let b):
-      return (a_ou).nnf && (b).nnf
     case .constant(let const):
       return const ? true : false
     case .proposition(_):
       return self
+    case .disjunction(let a_ou , let b):
+      return (a_ou).nnf && (b).nnf
+    case .conjunction(let a_et , let b):
+      return (a_et).nnf || (b).nnf
+    case .implication(let a_implique,let b):
+      return (!a_implique).nnf || (b).nnf
     case .negation(let formula):
 
           switch formula{
+            case .negation(let neg):
+              return neg.nnf
+            case .disjunction(let a_ou , let b):
+              return((!a_ou).nnf) && ((!b).nnf)
+            case .conjunction(let a_et , let b):
+              return ((!a_et).nnf) || ((!b).nnf)
+            case .implication(let a_implique,let b):
+              return (a_implique).nnf && (!b).nnf
             case .constant(let cons):
               return cons ? false : true
             case .proposition(_):
               return self
-            case .negation(let neg):
-              return neg.nnf
-            case .conjunction(let a_et , let b):
-              return ((!a_et).nnf) || ((!b).nnf)
-            case .disjunction(let a_ou , let b):
-              return((!a_ou).nnf) && ((!b).nnf)
-            case .implication(let a_implique,let b):
-              return (a_implique).nnf && (!b).nnf
         }
       }
     }
 
-  /// The disjunctive normal form (DNF) of the formula.
-  public var dnf: Formula {
-     switch self.nnf { //on fait un nnf
 
-//cas conjonction
-    case .conjunction:
-      var operands = self.nnf.conjunctionOperands
-    //but: separation en disjonction
-      if let firstDisjunction = operands.first(where: { //separation des operandes
-              if case .disjunction = $0 { //recherche de disjonction
-                return true //true si trouvé
-              }
-            return false }) //false sinon
-      {
-        if case let .disjunction(a,b) = firstDisjunction {
-          operands.remove(firstDisjunction) //suppression de l'opérande car redondance
-          var res : Formula?
-          for op in operands { //itération sur opérandes des opérandes
-            if res != nil {
-              res = res! && op //concaténation
-             }
-            else {
-              res = op
+
+public var dnf: Formula {
+
+
+        switch self.nnf {
+        case .conjunction(let a_et, let b_et):
+            switch (a_et.dnf, b_et.dnf) {
+            case (.disjunction(let a_ou, let b_ou), _):
+                return (a_ou.dnf && b_et.dnf) || (b_ou.dnf && b_et.dnf)
+            case (_, .disjunction(let a_ou, let b_ou)):
+                return (a_ou.dnf && a_et.dnf) || (b_ou.dnf && a_et.dnf)
+            default:
+                return a_et.dnf && b_et.dnf
             }
-          }
-        res = (a.dnf && res!.dnf) || (b.dnf && res!.dnf)
-          if res != nil { return res!.dnf}
-      }
-    }
-    return self.nnf
-//cas disjonction
-  case .disjunction: //
-    var operands = self.disjunctionOperands
-    for op in operands {
-      for op2 in operands {
-        if op.conjunctionOperands.isSubset(of: op2.conjunctionOperands) && op2 != op {
-          operands.remove(op2)
+        default:
+            return self.nnf
         }
-      }
-    }
-    var res : Formula?
-    for op in operands {
-      if res != nil {
-        res = res! || op
-      }
-      else {
-        res = op
-      }
-    }
-    return res!
 
-  default :
-    return self.nnf
     }
-  }
-  /// The conjunctive normal form (CNF) of the formula.
-  public var cnf: Formula {
-    switch self.nnf {
- //cas disjonction
 
-    case .disjunction:
-      var operandes = self.nnf.disjunctionOperands
-      if let firstConjunction = operandes.first(where: { if case  .conjunction = $0 {
-        return true
-      }
-      return false }) {
-          if case let .conjunction(a, b) = firstConjunction {
-            operandes.remove(firstConjunction)
-            var resultat : Formula?
-            for operandeA in operandes {
-              if resultat != nil  {
-                resultat = resultat! || operandeA
-              }
-              else {
-                resultat = operandeA
-              }
-            }
-          resultat = (a.cnf || resultat!.cnf) && (b.cnf || resultat!.cnf)
-          if resultat != nil { return resultat!.cnf}
-          }
-      }
-      return self.nnf
-  //cas conjonction
-    case .conjunction:
-      var operandes = self.nnf.conjunctionOperands
-      for operandeA in operandes {
-        for operandeB in operandes {
-          if operandeA.disjunctionOperands.isSubset(of:operandeB.disjunctionOperands) && operandeB != operandeA {
-            operandes.remove(operandeB)
-          }
-        }
-      }
-      var resultat : Formula?
-      for operandeA in operandes {
-        if resultat != nil  {
-          resultat = resultat! && operandeA
-        }
-        else {
-          resultat = operandeA
-        }
-      }
-      return resultat!
-    default :
-      return self.nnf
-    }
-  }
+public var cnf: Formula {
 
+       switch self.nnf {
+       case .disjunction(let a_ou, let b_ou):
+           switch (a_ou.cnf, b_ou.cnf) {
+           case (.conjunction(let a_et, let b_et), _):
+               return (a_et.cnf || b_ou.cnf) && (b_et.cnf || b_ou.cnf)
+           case (_, .conjunction(let a_et, let b_et)):
+               return (a_et.cnf || a_ou.cnf) && (b_et.cnf || a_ou.cnf)
+           default:
+               return a_ou.cnf || b_ou.cnf
+           }
+       default:
+           return self.nnf
+       }
+
+   }
 
 
   /// The minterms of a formula in disjunctive normal form.
   public var minterms: Set<Set<Formula>> {
-    switch self {
-
-    case .disjunction:
-      let operands = self.disjunctionOperands
-      var resultat : Set<Set<Formula>> = []
-      for operande in operands {
-        resultat.insert(operande.conjunctionOperands)
-      }
-      return resultat
-    default:
-      return []
-
-    }
-  }
-  /// The maxterms of a formula in conjunctive normal form.
-  public var maxterms: Set<Set<Formula>> {
-    switch self {
-
-      case .conjunction:
-        let operands = self.conjunctionOperands
-        var resultat : Set<Set<Formula>> = []
-        for operande in operands {
-            resultat.insert(operande.disjunctionOperands)
-          }
-          return resultat
+        switch self {
+        case .disjunction:
+            var result : Set<Set<Formula>> = []
+            for operand in self.disjunctionOperands {
+                result.insert(operand.conjunctionOperands)
+            }
+            return result
         default:
-          return []
+            return []
         }
     }
+  /// The maxterms of a formula in conjunctive normal form.
+  public var maxterms: Set<Set<Formula>> {
+        switch self {
+        case .conjunction:
+            var resultat : Set<Set<Formula>> = []
+            let operands = self.conjunctionOperands
+            for operande in operands {
+                resultat.insert(operande.disjunctionOperands)
+            }
+            return resultat
+        default:
+            return []
+        }
+    }
+
 
 
   /// Unfold a tree of binary disjunctions into a set of operands.
