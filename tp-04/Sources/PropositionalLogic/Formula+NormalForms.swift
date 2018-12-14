317c38fe187f9a50d42c9908986841162ffc482c
Laurent Roro <le.robynet.cool@gmail.com>
2018-12-14 20:09:23 +0100

    TP4

--- tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
+++ tp-04/Sources/PropositionalLogic/Formula+NormalForms.swift
@@ -33,10 +33,15 @@ extension Formula {
 						return c ? false : true
 					case .proposition(_):
 						return self // rien à développer
+
+					// De Morgan  ¬(A∨B)≡¬A∧¬B 
 					case .disjunction(let aa, let bb):
 						return (!aa).nnf && (!bb).nnf
+
+					// De Morgan  ¬(A∧B)≡¬A∨¬B
 					case .conjunction(let aa, let bb):
 						return (!aa).nnf || (!bb).nnf
+
 					case .implication(let aa, let bb):
 						return (aa).nnf && (!bb).nnf
 					case .negation(let nn):
@@ -46,43 +51,110 @@ extension Formula {
 	}
 
 	/// The disjunctive normal form (DNF) of the formula.
+	// but : mettre sous forme ((b ∧ ¬c ) ∨ (¬a ∧ ¬c))
 	public var dnf: Formula {
 
 		let nnf = self.nnf
-
 		// nnf n'a que des ∧, ∨, constant, ¬constant, proposition
-
-		// De Morgan        ¬(A∧B)≡¬A∨¬B
-		// De Morgan        ¬(A∨B)≡¬A∧¬B
 
 		// distributivité   A∨(B∧C)≡(A∨B)∧(A∨C)
 		// distributivité   A∧(B∨C)≡(A∧B)∨(A∧C)
 
-		// but : ((b ∧ ¬c) ∨ (¬a ∧ ¬c))
+		switch nnf {
+			case .conjunction(_,_):
+				var operands = nnf.conjunctionOperands
 
-		return nnf
+				// extract first disjunction and fight it
+				if let first_disjunction = operands.first( where: is_disjonction ) {
+					// that's a bit sad to write this way... I selected it to be a disjunction
+					if case let .disjunction(aa, bb) = first_disjunction {
+						// pop 
+						operands.remove( first_disjunction )
+
+						// we ∧ each operands left
+						let out = operands.reduce( true, {(acc, element) -> Formula in return acc && element})
+						// distributivité   A∧(B∨C)≡(A∧B)∨(A∧C)
+						return ((aa.dnf && out.dnf) || (bb.dnf && out.dnf)).dnf
+					}
+				}
+				// no bad guys around ! yaaay we're done !
+				return nnf
+
+			case .disjunction(_,_):
+				var operands = nnf.disjunctionOperands
+				// simplify
+				for operand in operands {
+					for op in operands {
+						if operand.conjunctionOperands.isSubset(of: op.conjunctionOperands) && operand != op {
+							operands.remove(op)
+						}
+					}
+				}
+
+				// `or` each remaining operands
+				return operands.reduce( false, {(acc, element) -> Formula in return acc || element})
+
+			default: // constant, ¬constant ...
+				return nnf
+		}
+
+
 	}
 	
 	/// The conjunctive normal form (CNF) of the formula.
+	// but : mettre sous forme ((b ∨ ¬c ) ∧ (¬a ∨ ¬c))
 	public var cnf: Formula {
-
 		let nnf = self.nnf
+		
+		switch nnf {
+			case .disjunction(_,_):
+				var operands = nnf.disjunctionOperands
 
-		return nnf
+				// extract first conjunction and fight it
+				if let first_conjunction = operands.first( where: is_conjonction ) {
+					// that's a bit sad to write this way... I selected it to be a conjunction
+					if case let .conjunction(aa, bb) = first_conjunction {
+						// pop
+						operands.remove( first_conjunction )
+
+						// we ∨ each operands left
+						let out = operands.reduce( false, {(acc, element) -> Formula in return acc || element})
+						// distributivité   A∨(B∧C)≡(A∨B)∧(A∨C)
+						return ((aa.cnf || out.cnf) && (bb.cnf || out.cnf)).cnf
+					}
+				}
+				// no bad guys around ! yaaay we're done !
+				return nnf
+
+			case .conjunction(_,_):
+				var operands = nnf.conjunctionOperands
+				// simplify
+				for operand in operands {
+					for op in operands {
+						if operand.disjunctionOperands.isSubset(of: op.disjunctionOperands) && operand != op {
+							operands.remove(op)
+						}
+					}
+				}
+
+				// `and` each remaining operands
+				return operands.reduce( true, {(acc, element) -> Formula in return acc && element})
+
+			default: // constant, ¬constant ...
+				return nnf
+		}
 	}
 	
 	/// The minterms of a formula in disjunctive normal form.
 	public var minterms: Set<Set<Formula>> {
-		let dnf = self.dnf
-		return Set(dnf.disjunctionOperands.map({
+		return Set(self.disjunctionOperands.map({
 			(operand) in return operand.conjunctionOperands
 		}))
 	}
 	
 	/// The maxterms of a formula in conjunctive normal form.
 	public var maxterms: Set<Set<Formula>> {
-		let dnf = self.dnf
-		return Set(dnf.conjunctionOperands.map({
+		return Set(self.conjunctionOperands.map({
 			(operand) in return operand.disjunctionOperands
 		}))
 	}
