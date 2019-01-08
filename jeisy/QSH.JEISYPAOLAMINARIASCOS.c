5585498139bb4366f56af99612a7aa55a090259e
andres <andres.caballero@etu.unige.ch>
2019-01-08 10:35:49 +0100

    jeisy

---
+++ jeisy/QSH.JEISYPAOLAMINARIASCOS.c
@@ -0,0 +1,72 @@
+Quik shoot 2019
+Jeisy Paola Mina Riascos
+
+job.1
+Latin Products SAS
+asistente contable
+27.12.2017 - 30.08.2018 
+
+job.2
+COOPERATIVA DE SERVIDORES PUBLICOS Y JUBILADOS DE COLOMBIA
+Auxiliar Contable 
+01.02.2017 - 30.06.2017
+
+job.3
+WIDEX COLOMBIA SAS
+Auxiliar de Contable 
+11.08.2016 – 28.11.2016 
+
+job.4
+GREEN HORIZON S.A.S 
+Asistente Administrativa y Financiera 
+11.02.2015- 04.12.2015 
+
+job.5
+PRODUCTOS QIKELY S.A.S. 
+Asistente de Cartera 
+04.08.2014- 04.02.2015 
+
+job.6
+MAKRO SUPERMAYORISTA S.A.S
+Cajera Servicio al cliente
+25.04.2012- 15.07.2014
+
+job.7
+ALMACES ÉXITO S.A. (Carulla)
+Auxiliar Cajas 
+01.07.2011- 28.12.2011 
+
+education.1
+Servicio Nacional de Aprendizaje SENA
+Pensiones y Parafiscales Básico 
+2015 
+
+education.2
+Institución Universitaria Antonio José Camacho 
+Diplomado en Gestión Humana en Contexto 
+2014 
+
+education.3
+Institución Universitaria Antonio Jose Camacho 
+Tecnología en Contabilidad Sistematizada 
+2012-2014 
+
+education.4
+Servicio Nacional de Aprendizaje SENA
+Documentación y Registro de Operaciones Contables 
+2010 
+
+education.5
+Institución Educativa Liceo del Pacifico 
+Bachiller Comercial 
+1999 - 2005 
+
+
+
+
+
+
+
+
+
+
